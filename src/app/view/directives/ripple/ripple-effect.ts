a8ae4a4e6cbed0b5e305d86a7648a8c5d622d7e2
Freddy Montes <freddy@dotcms.com>
2017-04-13 12:48:55 -0600

    Hit the dom less and improve animation

--- src/app/view/directives/ripple/ripple-effect.ts
+++ src/app/view/directives/ripple/ripple-effect.ts
@@ -14,63 +14,72 @@ import { ColorUtil } from '../../../api/util/ColorUtil';
  * Directives to ripple effects.
  * How to use:
  * <code>
- *  <button ripple pButton id="login-component-login-submit-button" *ngIf="!isLoginInProgress"
- *                           (click)="logInUser()" [label]="loginButton"></button>
+ *  <button ripple pButton id="login-component-login-submit-button" *ngIf="!isLoginInProgress" (click)="logInUser()" [label]="loginButton"></button>
  * </code>
  */
 @Directive({
     host: {
         '[style.overflow]': '"hidden"',
-        '[style.position]': '"relative"',
+        '[style.position]': '"relative"'
     },
     selector: '[ripple]'
 })
 export class DotRippleEffectDirective {
-
-    private static readonly WHITE_COLOR = 'rgb(255, 255, 255)';
-    private static readonly ANIMATION_DURATION_MILLIS = 650;
+    private static readonly WHITE_COLOR = 'rgba(255, 255, 255, 0.4)';
     private static readonly EFFECT_DEFAULT_COLOR = 'rgba(0, 0, 0, 0.2)';
+    private rippleElement: HTMLElement;
+    private rippleSize: RippleSize;
+    private hostNativeElement: HTMLElement;
 
     constructor(private host: ElementRef, private renderer: Renderer, private colorUtil: ColorUtil) {
+        this.hostNativeElement = host.nativeElement;
     }
 
     @HostListener('click', ['$event'])
-    onClick(event: any): void {
-        let hostNativeElement: any = this.host.nativeElement;
+    onClick(event: MouseEvent): void {
+        if (!this.rippleElement) {
+             this.createRippleElement();
+        }
 
-        let divElement = this.createDiv( event );
-
-        window.setTimeout(() => {
-            this.renderer.detachView([divElement]);
-        }, DotRippleEffectDirective.ANIMATION_DURATION_MILLIS);
+        this.setRipplePosition(event);
+        this.animateRipple();
     }
 
-    private createDiv(event: any): any {
-        let hostNativeElement: any = this.host.nativeElement;
-        let divElement = this.renderer.createElement(hostNativeElement, 'div');
+    private animateRipple(): void {
+        this.renderer.setElementClass(this.rippleElement, 'animate', false);
+        setTimeout(() => {
+            this.renderer.setElementClass(this.rippleElement, 'animate', true);
+        }, 1);
+    }
 
-        let btnOffset = hostNativeElement.getBoundingClientRect();
+    private createRippleElement(): void {
+        this.rippleElement = this.renderer.createElement(this.hostNativeElement, 'div');
+        this.rippleSize = this.getRippleSize();
+        this.renderer.setElementClass(this.rippleElement, 'ripple-effect', true);
+        this.renderer.setElementStyle(this.rippleElement, 'width', `${this.rippleSize.width}px`);
+        this.renderer.setElementStyle(this.rippleElement, 'height', `${this.rippleSize.height}px`);
+        this.renderer.setElementStyle(this.rippleElement, 'background', this.getRippleColor());
+    }
+
+    private getRippleColor(): string {
+        let hostBackgroundColor = window.getComputedStyle(this.hostNativeElement, null).backgroundColor;
+        let isBright = this.colorUtil.isBrightness(hostBackgroundColor);
+
+        return isBright ? DotRippleEffectDirective.WHITE_COLOR : DotRippleEffectDirective.EFFECT_DEFAULT_COLOR;
+    }
+
+    private getRipplePosition(event: MouseEvent): RipplePosition {
+        let btnOffset = this.hostNativeElement.getBoundingClientRect();
         let xPos = event.pageX - btnOffset.left;
         let yPos = event.pageY - btnOffset.top;
-        let hostHeight = hostNativeElement.clientHeight;
 
-        let divSize: Size = this.getDivSize();
-        let divLeft = (xPos - (divSize.width / 2));
-        let divTop = (yPos - (divSize.height / 2));
-
-        this.renderer.setElementClass(divElement, 'ripple-effect', true);
-        this.renderer.setElementStyle(divElement, 'position',  'absolute');
-        this.renderer.setElementStyle(divElement, 'border-radius',  '50%');
-        this.renderer.setElementStyle(divElement, 'width',  `${divSize.width}px`);
-        this.renderer.setElementStyle(divElement, 'height',  `${divSize.height}px`);
-        this.renderer.setElementStyle(divElement, 'background', this.getRippleEffectsColor());
-        this.renderer.setElementStyle(divElement, 'top', `${divTop}px`);
-        this.renderer.setElementStyle(divElement, 'left',  `${divLeft}px`);
-
-        return divElement;
+        return {
+            x: (xPos - (this.rippleSize.width / 2)),
+            y: (yPos - (this.rippleSize.height / 2))
+        };
     }
 
-    private getDivSize(): Size {
+    private getRippleSize(): RippleSize {
         let btnOffset = this.host.nativeElement.getBoundingClientRect();
         let rippleSize = Math.sqrt(btnOffset.width * btnOffset.width +
           btnOffset.height * btnOffset.height) * 2 + 2;
@@ -81,16 +90,19 @@ export class DotRippleEffectDirective {
         };
     }
 
-    private getRippleEffectsColor(): string {
-        let hostNativeElement: any = this.host.nativeElement;
-        let hostBackgroundColor = window.getComputedStyle(hostNativeElement, null).backgroundColor;
-
-        let isBright = this.colorUtil.isBrightness(hostBackgroundColor);
-        return !isBright ?  DotRippleEffectDirective.EFFECT_DEFAULT_COLOR : DotRippleEffectDirective.WHITE_COLOR;
+    private setRipplePosition(event: MouseEvent): void {
+        let ripplePosition: RipplePosition = this.getRipplePosition(event);
+        this.renderer.setElementStyle(this.rippleElement, 'top', `${ripplePosition.y}px`);
+        this.renderer.setElementStyle(this.rippleElement, 'left',  `${ripplePosition.x}px`);
     }
 }
 
-interface Size {
+interface RippleSize {
     height: number;
     width: number;
+}
+
+interface RipplePosition {
+    x: number;
+    y: number;
 }
