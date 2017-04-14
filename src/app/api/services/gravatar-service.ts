959b8234f088e5e629c6c68d7bfe4bc1a89463ef
woon tsou <woontsoudev@gmail.com>
2017-04-13 23:55:50 -0600

    dotCMS/core#10170 gravatar implementation v2

--- src/app/api/services/gravatar-service.ts
+++ src/app/api/services/gravatar-service.ts
@@ -12,18 +12,9 @@ export class GravatarService {
     constructor(private jsonp: Jsonp) {
     }
 
-    // loadGravatarProfile(email): Observable<any> {
-    //     return this.coreWebService.request({
-    //         method: RequestMethod.Get,
-    //         url: `https://www.gravatar.com/${md5(email)}.json`
-    //     });
-    // }
+    loadGravatarProfile(hash): Observable<any> {
 
-    loadGravatarProfile(email): Observable<any[]> {
-        // const _email = md5('woon.tsou@dotcms.com');
-        console.log('-----------------+++++++++++++++++==================');
-        console.log(email);
-        return this.jsonp.get(`http://www.gravatar.com/${email}.json?callback=JSONP_CALLBACK`).map(data => data.json());
+        return this.jsonp.get(`http://www.gravatar.com/${hash}.json?callback=JSONP_CALLBACK`)
+                            .map(data => data.json());
     }
-
 }
--- src/app/view/components/_common/gravatar/gravatar.component.ts
+++ src/app/view/components/_common/gravatar/gravatar.component.ts
@@ -10,19 +10,45 @@ let md5 = require('md5');
 
 export class GravatarComponent {
     @Input() email;
-    public profile;
+    @Input() size;
+    public gravatarProfileStyles;
+    public gravatarProfile;
+    public dotAvatarProfile;
+    public dotAvatarProfileStyles;
 
     constructor(private gravatarService: GravatarService) {
-        const _email = md5(this.email);
-        const profile$ = gravatarService.loadGravatarProfile(_email);
-        console.log(_email);
+
+    }
+
+    ngOnInit(): void {
+        const hash = md5(this.email);
+        const profile$ = this.gravatarService.loadGravatarProfile(hash);
         profile$.subscribe(
-            data => console.log(data),
-            error => console.error(error)
+            data => {
+                this.gravatarProfileStyles = {
+                    'background': 'url("' + data.entry[0].photos[0].value + '?s=' + this.size + '")',
+                    'border-radius': this.size / 2 + 'px',
+                    'height': this.size + 'px',
+                    'width': this.size + 'px'
+                };
+
+                this.gravatarProfile = true;
+            },
+            error => {
+                this.gravatarProfile = false;
+                this.dotAvatarProfile = this.setDotAvatar(this.email);
+                this.dotAvatarProfileStyles = {
+                    'border-radius': this.size / 2 + 'px',
+                    'font-size': this.size - ((this.size * 25) / 100) + 'px',
+                    'height': this.size + 'px',
+                    'width': this.size + 'px'
+                };
+            }
         );
     }
 
-    showTest(param): void {
-        console.log(param);
+    public setDotAvatar(email): any {
+        const firstLetter = email.substring(0, 1).toUpperCase();
+        return firstLetter;
     }
 }
