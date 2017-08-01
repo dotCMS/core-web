e4225dbf12b86c63404670726a91f49976ff202f
jtesser <jasontesser@gmail.com>
2017-08-01 15:36:34 -0400

    changes for npm publishing of dotcms-js

--- config/webpack.generate.dotcms-js.js
+++ config/webpack.generate.dotcms-js.js
@@ -1,37 +1,37 @@
-var path = require('path');
-var webpack = require('webpack');
-var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
-// var glob = require("glob");
+var path = require('path')
+var webpack = require('webpack')
+var WebpackBuildNotifierPlugin = require('webpack-build-notifier')
+// const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const PATHS = {
+    src: path.join(__dirname, '../src'),
+    build: path.join(__dirname, '../build-dotcms-js')
+}
 
 module.exports = {
-    devtool: 'source-map',
 
-    // entry: glob.sync("./src/**/*.ts"),
+    // entry: {
+    //     'dotcms-js': PATHS.src + '/main.browser.ts'
+    // },
     entry: {
-        'polyfills': './src/polyfills.browser.ts',
-        'main': './src/main.browser.ts'
+        '@angular': [
+            'rxjs',
+            'reflect-metadata',
+            'zone.js'
+        ],
+        'common': ['es6-shim'],
+        // 'app': './src/app/main.ts',
+        // 'vendor': './showcase/vendor.ts'
     },
-
     output: {
-        path: __dirname + '/../build-dotcms-js/',
+        path: PATHS.build,
         filename: '[name].js',
-        sourceMapFilename: '[name].js.map',
+        library: 'dotcms-js',
+        libraryTarget: 'umd'
     },
-
-    resolve: {
-        extensions: ['.ts', '.js', '.json', '.css', '.html']
-    },
-
+    devtool: 'source-map',
     module: {
-        loaders: [
-            {
-                test: /\.css$/,
-                use: ['to-string-loader', 'css-loader'],
-            },
-            {
-                test: /\.scss$/,
-                use: ['to-string-loader', 'css-loader', 'sass-loader'],
-            },
+        rules: [
             {
                 test: /\.ts$/,
                 loader: 'awesome-typescript-loader',
@@ -41,28 +41,42 @@ module.exports = {
                     }
             },
             {
-                test: /\.json$/,
-                loader: 'json-loader'
-            },
-            {
                 test: /\.(html)$/,
                 loader: 'raw-loader'
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader',
             },
             {
                 test: /\.(jpg|png|gif)$/,
                 use: 'file-loader?name=assets/img/[name]-[hash].[ext]'
             },
-
-            /* File loader for supporting fonts, for example, in CSS files.
-             */
+            {
+                test: /\.scss$/,
+                use: ['to-string-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.css$/,
+                use: ['to-string-loader', 'css-loader'],
+            },
             {
                 test: /\.(eot|woff2?|svg|ttf)([\?]?.*)$/,
                 use: 'file-loader?name=assets/fonts/[name]-[hash].[ext]'
             }
-            ]
+        ],
     },
-
+    resolve: {
+        // you can now require('file') instead of require('file.js')
+        extensions: ['.ts', '.js']
+    },
     plugins: [
-        new CommonsChunkPlugin({names: ['@angular', 'common'], minChunks: Infinity}),
+        new WebpackBuildNotifierPlugin({
+            title: 'My Project Webpack Build'
+        }),
+        // new ExtractTextPlugin('solar-popup.css'),
+        new webpack.IgnorePlugin(/test\.spec\.ts$/)
+
+
     ]
-};
+}
