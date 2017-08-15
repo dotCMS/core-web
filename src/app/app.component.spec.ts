d6c7bae9b1334221e169d92ca43311d18c31d62c
Freddy Montes <freddy@dotcms.com>
2017-08-15 15:47:08 -0600

    Fix unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,41 +1,39 @@
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 import { TestBed, async } from '@angular/core/testing';
 
+
 import { AppComponent } from './app.component';
+import { DOTTestBed } from './test/dot-test-bed';
+import { NotLicensedService } from './api/services/not-licensed-service';
+import { RoutingService } from './api/services/routing-service';
+import { LoginService } from './api/services/login-service';
+import { SocketFactory } from './api/services/protocol/socket-factory';
+import { DotRouterService } from './api/services/dot-router-service';
 
 describe('AppComponent', () => {
-    beforeEach(
-        async(() => {
-            TestBed.configureTestingModule({
-                declarations: [AppComponent]
-            }).compileComponents();
-        })
-    );
+    beforeEach(() => {
+        DOTTestBed.configureTestingModule({
+            declarations: [AppComponent],
+            imports: [RouterTestingModule.withRoutes([{ path: 'test', component: AppComponent }])],
+            providers: [
+                DotRouterService,
+                LoginService,
+                NotLicensedService,
+                RoutingService,
+                SocketFactory
+            ]
+        });
+    });
 
     it(
-        'should create the app',
+        'should have router-outlet',
         async(() => {
             const fixture = TestBed.createComponent(AppComponent);
             const app = fixture.debugElement.componentInstance;
-            expect(app).toBeTruthy();
-        })
-    );
-
-    it(
-        `should have as title 'app'`,
-        async(() => {
-            const fixture = TestBed.createComponent(AppComponent);
-            const app = fixture.debugElement.componentInstance;
-            expect(app.title).toEqual('app');
-        })
-    );
-
-    it(
-        'should render title in a h1 tag',
-        async(() => {
-            const fixture = TestBed.createComponent(AppComponent);
-            fixture.detectChanges();
-            const compiled = fixture.debugElement.nativeElement;
-            expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+            const de: DebugElement = fixture.debugElement.query(By.css('router-outlet'));
+            expect(de).not.toBeNull();
         })
     );
 });
