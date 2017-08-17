8c897a217cd47835a4f025d47a747adbd7f79268
freddyrodriguez <freddy0309@gmail.com>
2017-08-17 11:30:17 -0600

    dotCMS/core#12193 Adding property to defalult-value property

--- src/app/portlets/content-types/fields/content-type-fields-properties-form/field-properties/default-value-property/default-value-property.component.ts
+++ src/app/portlets/content-types/fields/content-type-fields-properties-form/field-properties/default-value-property/default-value-property.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { FieldProperty } from '../field-properties.interface';
 import { BaseComponent } from '../../../../../../view/components/_common/_base/base-component';
 import { MessageService } from '../../../../../../api/services/messages-service';
+import { FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'default-value-property',
@@ -9,6 +10,7 @@ import { MessageService } from '../../../../../../api/services/messages-service'
 })
 export class DefaultValuePropertyComponent extends BaseComponent {
     property: FieldProperty;
+    group: FormGroup;
 
     constructor(public messageService: MessageService) {
         super(
