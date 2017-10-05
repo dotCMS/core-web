b9bebe93dd7eca116b8be08e5677bcec5bc82bed
Luis Rojas <luis.rojas@dotcms.com>
2017-10-05 08:53:28 -0600

    dotCMS/core#12743 Removing space

--- src/app/api/services/dot-confirmation/dot-confirmation.service.spec.ts
+++ src/app/api/services/dot-confirmation/dot-confirmation.service.spec.ts
@@ -16,11 +16,9 @@ describe('DotConfirmationService', () => {
         };
 
         this.dotConfirmationService =  this.injector.get(DotConfirmationService);
-
     });
 
     it('should emit data to labels property', () => {
-
         this.dotConfirmationService.confirm(mockData);
 
         this.dotConfirmationService.labels.subscribe((message) => {
@@ -29,17 +27,14 @@ describe('DotConfirmationService', () => {
                 rejectLabel: 'Reject'
             });
         });
-
     });
 
     it('should call confirmation service with data parameter', () => {
-
         spyOn(this.dotConfirmationService.confirmationService, 'confirm');
 
         this.dotConfirmationService.confirm(mockData);
 
         expect(this.dotConfirmationService.confirmationService.confirm).toHaveBeenCalledWith(mockData);
     });
-
 });
 
