e6f29132ed2d7f207b30c9c5ecf6d7552480f2df
Humberto Morera <humberto.morera@dotcms.com>
2017-11-02 14:23:41 -0600

    merge master

--- src/app/portlets/content-types/main/content-types.component.ts
+++ src/app/portlets/content-types/main/content-types.component.ts
@@ -4,13 +4,15 @@ import { DotConfirmationService } from './../../../api/services/dot-confirmation
 import { CrudService } from './../../../api/services/crud';
 import { MenuItem } from 'primeng/primeng';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { ActionHeaderOptions } from '../../../shared/models/action-header';
 import { BaseComponent } from '../../../view/components/_common/_base/base-component';
 import { ContentTypesInfoService } from '../../../api/services/content-types-info';
 import { DataTableColumn } from '../../../shared/models/data-table';
 import { MessageService } from '../../../api/services/messages-service';
+import { Observable } from 'rxjs/Observable';
+import { DotContentletService } from '../../../api/services/dot-contentlet.service';
 
 /**
  * List of Content Types
@@ -24,133 +26,107 @@ import { MessageService } from '../../../api/services/messages-service';
     styleUrls: ['./content-types.component.scss'],
     templateUrl: 'content-types.component.html'
 })
-export class ContentTypesPortletComponent extends BaseComponent {
+export class ContentTypesPortletComponent implements OnInit {
     public contentTypeColumns: DataTableColumn[];
     public item: any;
     public actionHeaderOptions: ActionHeaderOptions;
     public rowActions: MenuItem[];
+    public i18nMessages = {};
+    private i18nKeys = [
+        'contenttypes.fieldname.structure.name',
+        'contenttypes.content.variable',
+        'contenttypes.form.label.description',
+        'contenttypes.fieldname.entries',
+        'message.structure.delete.structure.and.content',
+        'message.structure.cantdelete',
+        'contenttypes.content.fileasset',
+        'contenttypes.content.content',
+        'contenttypes.content.persona',
+        'contenttypes.content.widget',
+        'contenttypes.content.htmlpage',
+        'contenttypes.content.key_value',
+        'contenttypes.content.vanity_url',
+        'contenttypes.content.form',
+        'contenttypes.confirm.message.delete',
+        'contenttypes.confirm.message.delete.content',
+        'contenttypes.confirm.message.delete.warning',
+        'contenttypes.action.delete',
+        'contenttypes.action.cancel',
+        'Content-Type'
+    ];
     @ViewChild('listing') listing: ListingDataTableComponent;
 
     constructor(
-        messageService: MessageService,
+        private messageService: MessageService,
         private router: Router,
         private route: ActivatedRoute,
         private contentTypesInfoService: ContentTypesInfoService,
         private crudService: CrudService,
-        private dotConfirmationService: DotConfirmationService
-    ) {
-        super(
-            [
-                'contenttypes.fieldname.structure.name',
-                'contenttypes.content.variable',
-                'contenttypes.form.label.description',
-                'contenttypes.fieldname.entries',
-                'message.structure.delete.structure.and.content',
-                'message.structure.cantdelete',
-                'contenttypes.content.file',
-                'contenttypes.content.content',
-                'contenttypes.content.persona',
-                'contenttypes.content.widget',
-                'contenttypes.content.page',
-                'contenttypes.confirm.message.delete',
-                'contenttypes.confirm.message.delete.content',
-                'contenttypes.confirm.message.delete.warning',
-                'contenttypes.action.delete',
-                'contenttypes.action.cancel',
-                'Content-Type'
-            ],
-            messageService
-        );
-    }
+        private dotConfirmationService: DotConfirmationService,
+        private dotContentletService: DotContentletService
+    ) {}
 
-    /**
-     * Callback call from BaseComponent when the messages are received.
-     * @memberOf ContentTypesPortletComponent
-     */
-    onMessage(): void {
-        this.actionHeaderOptions = {
-            primary: {
-                command: $event => {
-                    this.createContentType($event);
+    ngOnInit() {
+        Observable.forkJoin(
+            this.messageService.getMessages(this.i18nKeys),
+            this.dotContentletService.getContentTypes()
+        ).subscribe(val => {
+            this.i18nMessages = val[0];
+            this.actionHeaderOptions = {
+                primary: {
+                    command: $event => {
+                        this.createContentType($event);
+                    },
+                    model: []
+                }
+            };
+            val[1].filter(r => ['RECENT_CONTENT', 'RECENT_WIDGET'].indexOf(r.name) === -1).forEach(obj => {
+                this.actionHeaderOptions.primary.model.push({
+                    command: $event => {
+                        this.createContentType(obj.name.toLocaleLowerCase(), $event);
+                    },
+                    icon: this.contentTypesInfoService.getIcon(obj.name),
+                    label: this.i18nMessages['contenttypes.content.' + obj.name.toLocaleLowerCase()]
+                });
+            });
+            this.contentTypeColumns = [
+                {
+                    fieldName: 'name',
+                    header: this.i18nMessages['contenttypes.fieldname.structure.name'],
+                    icon: (item: any): string => this.contentTypesInfoService.getIcon(item.baseType),
+                    sortable: true
                 },
-                model: [
-                    {
-                        command: $event => {
-                            this.createContentType('content', $event);
-                        },
-                        icon: 'fa-newspaper-o',
-                        label: this.i18nMessages['contenttypes.content.content']
-                    },
-                    {
-                        command: $event => {
-                            this.createContentType('widget', $event);
-                        },
-                        icon: 'fa-cog',
-                        label: this.i18nMessages['contenttypes.content.widget']
-                    },
-                    {
-                        command: $event => {
-                            this.createContentType('file', $event);
-                        },
-                        icon: 'fa-file-o',
-                        label: this.i18nMessages['contenttypes.content.file']
-                    },
-                    {
-                        command: $event => {
-                            this.createContentType('page', $event);
-                        },
-                        icon: 'fa-file-text-o',
-                        label: this.i18nMessages['contenttypes.content.page']
-                    },
-                    {
-                        command: $event => {
-                            this.createContentType('persona', $event);
-                        },
-                        icon: 'fa-user',
-                        label: this.i18nMessages['contenttypes.content.persona']
-                    }
-                ]
-            }
-        };
-
-        this.contentTypeColumns = [
-            {
-                fieldName: 'name',
-                header: this.i18nMessages['contenttypes.fieldname.structure.name'],
-                icon: (item: any): string => this.contentTypesInfoService.getIcon(item.baseType),
-                sortable: true
-            },
-            {
-                fieldName: 'variable',
-                header: this.i18nMessages['contenttypes.content.variable'],
-                sortable: true
-            },
-            {
-                fieldName: 'description',
-                header: this.i18nMessages['contenttypes.form.label.description'],
-                sortable: true
-            },
-            {
-                fieldName: 'nEntries',
-                header: this.i18nMessages['contenttypes.fieldname.entries'],
-                width: '7%'
-            },
-            {
-                fieldName: 'modDate',
-                format: 'date',
-                header: 'Last Edit Date',
-                sortable: true,
-                width: '13%'
-            }
-        ];
-
-        this.rowActions = [
-            {
-                label: 'Remove',
-                icon: 'fa-trash',
-                command: (item) => this.removeConfirmation(item)
-            }
-        ];
+                {
+                    fieldName: 'variable',
+                    header: this.i18nMessages['contenttypes.content.variable'],
+                    sortable: true
+                },
+                {
+                    fieldName: 'description',
+                    header: this.i18nMessages['contenttypes.form.label.description'],
+                    sortable: true
+                },
+                {
+                    fieldName: 'nEntries',
+                    header: this.i18nMessages['contenttypes.fieldname.entries'],
+                    width: '7%'
+                },
+                {
+                    fieldName: 'modDate',
+                    format: 'date',
+                    header: 'Last Edit Date',
+                    sortable: true,
+                    width: '13%'
+                }
+            ];
+            this.rowActions = [
+                {
+                    label: 'Remove',
+                    icon: 'fa-trash',
+                    command: item => this.removeConfirmation(item)
+                }
+            ];
+        });
     }
 
     private createContentType(type: string, $event?): void {
