ca0ff1a7465b44b0817b866cb3b9eb4cf72d4ca0
Luis Rojas <luis.rojas@dotcms.com>
2017-11-13 17:28:52 -0600

    dotCMS/core#13005

--- src/app/view/components/_common/searchable-dropdown/component/searchable-dropdown.component.ts
+++ src/app/view/components/_common/searchable-dropdown/component/searchable-dropdown.component.ts
@@ -45,6 +45,7 @@ export class SearchableDropdownComponent extends BaseComponent implements Contro
     @Input() rows: number;
     @Input() totalRecords: number;
     @Input() placeholder = '';
+    @Input() persistentPlaceholder: string;
     @Input() width: string;
     @Output() change: EventEmitter<any> = new EventEmitter();
     @Output() filterChange: EventEmitter<string> = new EventEmitter();
