23fe3ea2aa9cf3879a8b683978839014f9476030
Humberto Morera <humberto.morera@dotcms.com>
2017-11-15 13:44:37 -0600

    adding testing

--- src/app/portlets/dot-edit-page/layout/dot-edit-layout-grid/dot-edit-layout-grid.component.spec.ts
+++ src/app/portlets/dot-edit-page/layout/dot-edit-layout-grid/dot-edit-layout-grid.component.spec.ts
@@ -1,25 +1,65 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture } from '@angular/core/testing';
+
+import { NgGridModule } from 'angular2-grid';
+import { Input, Component } from '@angular/core';
 
 import { DotEditLayoutGridComponent } from './dot-edit-layout-grid.component';
+import { DOTTestBed } from '../../../../test/dot-test-bed';
+
+
+@Component({
+    selector: 'action-button',
+    template: ''
+})
+
+class TestActionButtonComponent {
+    @Input() command?: ($event) => void;
+}
+
 
 describe('DotEditLayoutGridComponent', () => {
-  let component: DotEditLayoutGridComponent;
-  let fixture: ComponentFixture<DotEditLayoutGridComponent>;
+    let component: DotEditLayoutGridComponent;
+    let fixture: ComponentFixture<DotEditLayoutGridComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DotEditLayoutGridComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(() => {
+        DOTTestBed.configureTestingModule({
+            declarations: [DotEditLayoutGridComponent,TestActionButtonComponent],
+            imports:[ NgGridModule]
+        });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DotEditLayoutGridComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+         fixture = DOTTestBed.createComponent(DotEditLayoutGridComponent);
+         component = fixture.componentInstance;
+         fixture.detectChanges();
+    });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+
+    it('should show set one element in the grid of 12 columns', () => {
+        expect(component.gridContainers.length).toEqual(1);
+        expect(component.gridContainers[0].config.sizex).toEqual(12);
+    });
+
+    it('should add one Container to the grid of 3 columns', () => {
+        component.addBox();
+        expect(component.gridContainers.length).toEqual(2);
+        expect(component.gridContainers[1].config.sizex).toEqual(3);
+    });
+
+    it('should add a new Container in the same row', () => {
+        component.addBox();
+        component.addBox();
+        expect(component.gridContainers.length).toEqual(3);
+        expect(component.gridContainers[2].config.row).toEqual(2);
+    });
+
+    it('should add a new Container in a new row, when there is no space in the last row', () => {
+        component.addBox();
+        expect(component.gridContainers.length).toEqual(2);
+        expect(component.gridContainers[1].config.row).toEqual(2);
+    });
+
+    it('should remove one Container from the Grid', () => {
+        component.addBox();
+        component.removeContainer(0);
+        expect(component.gridContainers.length).toEqual(1);
+    });
 });
--- src/app/portlets/dot-edit-page/layout/dot-edit-layout-grid/dot-edit-layout-grid.component.ts
+++ src/app/portlets/dot-edit-page/layout/dot-edit-layout-grid/dot-edit-layout-grid.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NgGridConfig, NgGridItemConfig } from 'angular2-grid';
+import _ from 'lodash';
 
 //Final Object need to be defined.
-interface DotLayoutBlock {
+interface DotLayoutContainer {
     id: string;
-    config: any;
+    config: NgGridItemConfig;
 }
 
 @Component({
@@ -13,7 +14,7 @@ interface DotLayoutBlock {
     styleUrls: ['./dot-edit-layout-grid.component.scss']
 })
 export class DotEditLayoutGridComponent implements OnInit {
-    public gridContainers: Array<DotLayoutBlock> = [];
+    public gridContainers: Array<DotLayoutContainer> = [];
     private static maxColumns: number = 12;
     private static newRowTemplate: NgGridItemConfig = { fixed: true, sizex: 3, maxCols: 12, maxRows: 1 };
     private static defaultEmptyGridRows: NgGridItemConfig = {
@@ -52,6 +53,7 @@ export class DotEditLayoutGridComponent implements OnInit {
     constructor() {}
 
     ngOnInit() {
+        console.log('test');
         this.gridContainers = [
             {
                 id: Math.random().toString(),
@@ -64,11 +66,11 @@ export class DotEditLayoutGridComponent implements OnInit {
     }
 
     addBox(): void {
-        let conf: NgGridItemConfig = this.getLastContainerOnGrid();
+        let conf: NgGridItemConfig = this.setConfigOfNewContainer();
         this.gridContainers.push({ id: Math.random().toString(), config: conf });
     }
 
-    removeWidget(index: number): void {
+    removeContainer(index: number): void {
         if (this.gridContainers[index]) {
             this.gridContainers.splice(index, 1);
             this.deleteEmptyRows();
@@ -79,20 +81,21 @@ export class DotEditLayoutGridComponent implements OnInit {
         this.deleteEmptyRows();
     }
 
-    private getLastContainerOnGrid(): NgGridItemConfig {
+    private setConfigOfNewContainer(): NgGridItemConfig {
         let lastContainer;
         let newRow: NgGridItemConfig = Object.assign({}, DotEditLayoutGridComponent.newRowTemplate);
         if (this.gridContainers.length) {
+            //TODO: Try to make this simpler with lodash.
             // check last row && last column in last row
             lastContainer = this.gridContainers.reduce((a, b) => {
                 return a.config.row > b.config.row
                     ? a
                     : a.config.row == b.config.row ? (a.config.col > b.config.col ? a : b) : b;
             });
-            if (
-                lastContainer.config.col + DotEditLayoutGridComponent.newRowTemplate.sizex <
+            if ((lastContainer.config.col-1 + lastContainer.config.sizex +
+                    DotEditLayoutGridComponent.newRowTemplate.sizex) <=
                 DotEditLayoutGridComponent.maxColumns
-            ) {
+            ){
                 newRow.row = lastContainer.config.row;
                 newRow.col = lastContainer.config.col + lastContainer.config.sizex;
             } else {
@@ -103,31 +106,26 @@ export class DotEditLayoutGridComponent implements OnInit {
         return newRow;
     }
 
-    /**
-     *
-     */
     private deleteEmptyRows(): void {
+        //TODO: Find a solution to remove setTimeOut
         setTimeout(() => {
-            let containersByRow = this.sortContainersByRow(this.gridContainers);
-            let filteredRows = containersByRow.filter(contArr => contArr);
-
-            // If is the same size don't re-Order nothing.
-            if (filteredRows.length != containersByRow.length - 1) {
-                filteredRows.forEach((containerArr, index) => {
-                    if (containerArr[0].config.row != index + 1)
-                        containerArr.forEach(container => (container.config.row = index + 1));
-                });
-            }
+            this.gridContainers = _.chain(this.gridContainers)
+                .sortBy('config.row')
+                .groupBy('config.row')
+                .values()
+                .map(this.updateContainerIndex)
+                .flatten()
+                .value();
         }, 0);
     }
 
-    private sortContainersByRow(containers: Array<DotLayoutBlock>): Array<DotLayoutBlock[]> {
-        let containersByRow = [];
-        containers.forEach(container => {
-            containersByRow[container.config.row] === undefined
-                ? (containersByRow[container.config.row] = new Array(container))
-                : containersByRow[container.config.row].push(container);
-        });
-        return containersByRow;
+    private updateContainerIndex(rowArray, index) {
+        if (rowArray[0].row != index + 1) {
+            return rowArray.map(container => {
+                container.config.row = index + 1;
+                return container;
+            });
+        }
+        return rowArray;
     }
 }
