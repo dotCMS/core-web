65c84f077904a310177d4e21e2b3f4ba6dc63503
Freddy Montes <freddy@dotcms.com>
2017-11-27 11:57:34 -0600

    Fix special screnario for add boxes in the right place

--- src/app/portlets/dot-edit-page/layout/dot-edit-layout-grid/dot-edit-layout-grid.component.ts
+++ src/app/portlets/dot-edit-page/layout/dot-edit-layout-grid/dot-edit-layout-grid.component.ts
@@ -71,7 +71,6 @@ export class DotEditLayoutGridComponent implements OnInit {
         this.grid = this.isHaveRows(this.pageView)
             ? this.dotEditLayoutService.getDotLayoutGridBox(this.pageView)
             : [... DOT_LAYOUT_DEFAULT_GRID];
-
     }
 
     /**
@@ -129,7 +128,13 @@ export class DotEditLayoutGridComponent implements OnInit {
         const newRow: NgGridItemConfig = Object.assign({}, DOT_LAYOUT_GRID_NEW_ROW_TEMPLATE);
 
         if (this.grid.length) {
-            const lastContainer = _.last(_.sortBy(this.grid, 'config.row'));
+            const lastContainer = _.chain(this.grid)
+                .groupBy('config.row')
+                .values()
+                .last()
+                .maxBy('config.col')
+                .value();
+
             let busyColumns: number = DOT_LAYOUT_GRID_NEW_ROW_TEMPLATE.sizex;
 
             busyColumns += lastContainer.config.col - 1 + lastContainer.config.sizex;
