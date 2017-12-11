b3e047dec70ecffc73986502b13778e09f253dd5
Freddy Montes <freddy@dotcms.com>
2017-12-11 11:08:23 -0600

    Add the edit component to the crhome and style

--- src/app/portlets/dot-edit-content/dot-edit-content.module.ts
+++ src/app/portlets/dot-edit-content/dot-edit-content.module.ts
@@ -5,6 +5,7 @@ import { DialogModule } from 'primeng/primeng';
 import { IFrameModule } from '../../view/components/_common/iframe/index';
 import { Routes, RouterModule } from '@angular/router';
 import { DotContainerContentletService } from './services/dot-container-contentlet.service';
+import { DotEditPageToolbarModule } from './components/dot-edit-page-toolbar/dot-edit-page-toolbar.module';
 
 const routes: Routes = [
     {
@@ -16,7 +17,7 @@ const routes: Routes = [
 
 @NgModule({
     declarations: [DotEditContentComponent],
-    imports: [CommonModule, DialogModule, RouterModule.forChild(routes)],
+    imports: [CommonModule, DialogModule, RouterModule.forChild(routes), DotEditPageToolbarModule],
     exports: [DotEditContentComponent],
     providers: [DotContainerContentletService]
 })
