ef50d22dd2e3fb89b4d9457789a5092d5462a5a4
Luis Rojas <luis.rojas@dotcms.com>
2017-12-20 09:10:28 -0600

    Adding dot-edit-right-toolbar component q :q

---
+++ src/app/view/components/dot-edit-right-toolbar/dot-edit-right-toolbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'dot-edit-right-toolbar',
+    templateUrl: './dot-edit-right-toolbar.component.html',
+    styleUrls: ['./dot-edit-right-toolbar.component.scss'],
+})
+export class DotEditRightToolbarComponent { }
---
+++ src/app/view/components/dot-edit-right-toolbar/dot-edit-right-toolbar.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { DotEditRightToolbarComponent } from './dot-edit-right-toolbar.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+    declarations: [DotEditRightToolbarComponent],
+    imports: [CommonModule],
+    exports: [DotEditRightToolbarComponent],
+    providers: []
+})
+export class DotEditRightToolbarModule {}
