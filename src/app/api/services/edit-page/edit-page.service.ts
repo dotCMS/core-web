0650d86d3f37595b7f83367f5b67d2da6064ae4e
Freddy Montes <freddy@dotcms.com>
2017-12-28 07:43:46 -0600

    dotCMS/core#13261 Bring in new edit mode to core - Edit iframe component to listen to edit-page event - Edit router service to go to edit page

---
+++ src/app/api/services/edit-page/edit-page.service.ts
@@ -0,0 +1,25 @@
+import { CoreWebService } from 'dotcms-js/dotcms-js';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { RequestMethod } from '@angular/http';
+
+/**
+ * Provide util methods to get a edit page html
+ * @export
+ * @class EditPageService
+ */
+@Injectable()
+export class EditPageService {
+    constructor(private coreWebService: CoreWebService) {}
+
+    get(url: string): Observable<string> {
+        url = url || 'about-us/index';
+
+        return this.coreWebService
+            .requestView({
+                method: RequestMethod.Get,
+                url: `v1/page/renderHTML/${url.replace(/^\//, '')}?mode=EDIT_MODE`,
+            })
+            .pluck('bodyJsonObject', 'render');
+    }
+}
