c6b2726e6f1dfc16e3abd70ad34f745561e15591
Alfredo Li <alfredo@dotcms.com>
2018-03-11 20:13:07 -0600

    Fix Change subscribe call for async pipe

--- src/app/portlets/dot-edit-page/content/components/dot-edit-page-workflows-actions/dot-edit-page-workflows-actions.component.ts
+++ src/app/portlets/dot-edit-page/content/components/dot-edit-page-workflows-actions/dot-edit-page-workflows-actions.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MenuItem } from 'primeng/primeng';
 import { Workflow } from '../../../../../shared/models/workflow/workflow.model';
 import { WorkflowService } from '../../../../../api/services/workflow/workflow.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
     selector: 'dot-edit-page-workflows-actions',
@@ -12,19 +13,18 @@ export class DotEditPageWorkflowsActionsComponent implements OnInit {
     @Input() label: string;
 
     pageWorkflows: Workflow[];
-    workflowsActions: MenuItem[] = [];
+    workflowsActions: Observable<MenuItem[]>;
 
     constructor(private workflowsService: WorkflowService) {}
 
     ngOnInit() {
-        this.workflowsService.getPageWorkflows(this.inode).subscribe((data) => {
-            this.pageWorkflows = data;
-            this.workflowsActions = this.getWorkflowOptions();
-        });
+        this.workflowsActions = this.workflowsService
+            .getPageWorkflows(this.inode)
+            .map((workflows: Workflow[]) => this.getWorkflowOptions(workflows));
     }
 
-    private getWorkflowOptions(): MenuItem[] {
-        return this.pageWorkflows.map((workflow: Workflow) => {
+    private getWorkflowOptions(workflows: Workflow[]): MenuItem[] {
+        return workflows.map((workflow: Workflow) => {
             return {
                 label: workflow.name
             };
