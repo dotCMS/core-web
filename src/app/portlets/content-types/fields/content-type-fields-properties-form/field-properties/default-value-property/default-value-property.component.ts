9a795ca94168ca2850b81b2ff0cb702795b46d2d
Humberto Morera <humberto.morera@dotcms.com>
2018-04-18 15:31:01 -0500

    clean up

--- src/app/portlets/content-types/fields/content-type-fields-properties-form/field-properties/default-value-property/default-value-property.component.ts
+++ src/app/portlets/content-types/fields/content-type-fields-properties-form/field-properties/default-value-property/default-value-property.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FieldProperty } from '../field-properties.model';
-import { BaseComponent } from '../../../../../../view/components/_common/_base/base-component';
 import { DotMessageService } from '../../../../../../api/services/dot-messages-service';
 import { FormGroup } from '@angular/forms';
 
