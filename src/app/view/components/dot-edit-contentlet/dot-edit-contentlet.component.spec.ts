34c0258eeada183e1503812a8b7c04c9f8724be9
Freddy Montes <freddy@dotcms.com>
2018-05-10 13:02:07 -0600

    Implement and test dot-edit-contentlet

--- src/app/view/components/dot-edit-contentlet/dot-edit-contentlet.component.spec.ts
+++ src/app/view/components/dot-edit-contentlet/dot-edit-contentlet.component.spec.ts
@@ -7,6 +7,13 @@ import { DotEditContentletComponent } from './dot-edit-contentlet.component';
 import { DOTTestBed } from '../../../test/dot-test-bed';
 import { DotIframeDialogModule } from '../dot-iframe-dialog/dot-iframe-dialog.module';
 import { DotIframeDialogComponent } from '../dot-iframe-dialog/dot-iframe-dialog.component';
+import { LoginService } from 'dotcms-js/dotcms-js';
+import { LoginServiceMock } from '../../../test/login-service.mock';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DotMenuService } from '../../../api/services/dot-menu.service';
+import { IfObservable } from 'rxjs/observable/IfObservable';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
     selector: 'dot-test-host-component',
@@ -16,20 +23,34 @@ class TestHostComponent {
     inode: string;
 }
 
-
-describe('DotEditContentletComponent', () => {
+fdescribe('DotEditContentletComponent', () => {
     let component: DotEditContentletComponent;
     let de: DebugElement;
     let hostComponent: TestHostComponent;
     let hostDe: DebugElement;
     let hostFixture: ComponentFixture<TestHostComponent>;
 
+    let dotIframeDialog: DebugElement;
     let dotIframeDialogComponent: DotIframeDialogComponent;
 
     beforeEach(async(() => {
         DOTTestBed.configureTestingModule({
             declarations: [DotEditContentletComponent, TestHostComponent],
-            imports: [DotIframeDialogModule]
+            providers: [
+                {
+                    provide: LoginService,
+                    useClass: LoginServiceMock
+                },
+                {
+                    provide: DotMenuService,
+                    useValue: {
+                        getDotMenuId() {
+                            return Observable.of('999');
+                        }
+                    }
+                }
+            ],
+            imports: [DotIframeDialogModule, RouterTestingModule, BrowserAnimationsModule]
         }).compileComponents();
     }));
 
@@ -37,18 +58,49 @@ describe('DotEditContentletComponent', () => {
         hostFixture = DOTTestBed.createComponent(TestHostComponent);
         hostDe = hostFixture.debugElement;
         hostComponent = hostFixture.componentInstance;
-        de = hostDe.query(By.css('dot-dot-edit-contentlet'));
+        de = hostDe.query(By.css('dot-edit-contentlet'));
         component = de.componentInstance;
     });
 
-    describe('basic', () => {
+    describe('default', () => {
         beforeEach(() => {
             hostFixture.detectChanges();
-            dotIframeDialogComponent = de.query(By.css('dot-iframe-dialog')).componentInstance;
+            dotIframeDialog = de.query(By.css('dot-iframe-dialog'));
         });
 
-        it('should have DotIframeDialogComponent', () => {
-            expect(dotIframeDialogComponent).toBeFalsy();
+        it('should not have dot-iframe-dialog', () => {
+            expect(dotIframeDialog === null).toBe(true);
+        });
+    });
+
+    fdescribe('with inode', () => {
+        beforeEach(() => {
+            hostComponent.inode = '123';
+            hostFixture.detectChanges();
+            dotIframeDialog = de.query(By.css('dot-iframe-dialog'));
+            dotIframeDialogComponent = dotIframeDialog.componentInstance;
+        });
+
+        describe('dot-iframe-dialog', () => {
+            it('should have dot-iframe-dialog', () => {
+                expect(dotIframeDialog).toBeTruthy();
+            });
+
+            it('should have set url to', () => {
+                expect(dotIframeDialogComponent.url).toBe(
+                    [
+                        `/c/portal/layout`,
+                        `?p_l_id=999`,
+                        `&p_p_id=content`,
+                        `&p_p_action=1`,
+                        `&p_p_state=maximized`,
+                        `&angularCurrentPortlet=content`,
+                        `&p_p_mode=view`,
+                        `&_content_struts_action=%2Fext%2Fcontentlet%2Fedit_contentlet`,
+                        `&_content_cmd=edit&inode=123`
+                    ].join('')
+                );
+            });
         });
     });
 });
--- src/app/view/components/dot-edit-contentlet/dot-edit-contentlet.component.ts
+++ src/app/view/components/dot-edit-contentlet/dot-edit-contentlet.component.ts
@@ -1,14 +1,42 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, SimpleChanges, OnChanges } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { DotMenuService } from '../../../api/services/dot-menu.service';
 
 @Component({
     selector: 'dot-edit-contentlet',
     templateUrl: './dot-edit-contentlet.component.html',
     styleUrls: ['./dot-edit-contentlet.component.scss']
 })
-export class DotEditContentletComponent implements OnInit {
+export class DotEditContentletComponent implements OnChanges {
     @Input() inode: string;
 
-    constructor() {}
+    url: Observable<string>;
 
-    ngOnInit() {}
+    constructor(private dotMenuService: DotMenuService) {}
+
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes.inode.currentValue) {
+            this.url = this.getUrl(changes.inode.currentValue);
+        }
+    }
+
+    private getUrl(inode: string): Observable<string> {
+        return this.dotMenuService
+            .getDotMenuId('content')
+            .take(1)
+            .map((portletId: string) => {
+                return [
+                    `/c/portal/layout`,
+                    `?p_l_id=${portletId}`,
+                    `&p_p_id=content`,
+                    `&p_p_action=1`,
+                    `&p_p_state=maximized`,
+                    `&angularCurrentPortlet=content`,
+                    `&p_p_mode=view`,
+                    `&_content_struts_action=%2Fext%2Fcontentlet%2Fedit_contentlet`,
+                    `&_content_cmd=edit&inode=${inode}`
+                ].join('');
+            });
+        // return `some/thing/${inode}`;
+    }
 }
