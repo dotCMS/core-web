e24e24071864f4fa27baa82b793fa660e084694e
Alfredo Li <alfredo@dotcms.com>
2018-05-15 13:41:17 -0600

    Remove unnecesary code

--- src/app/portlets/dot-edit-page/main/dot-edit-page-main/dot-edit-page-main.component.ts
+++ src/app/portlets/dot-edit-page/main/dot-edit-page-main/dot-edit-page-main.component.ts
@@ -1,39 +1,19 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { DotRenderedPageState } from '../../shared/models/dot-rendered-page-state.model';
-import { Subscription } from 'rxjs/Subscription';
-import { filter } from 'rxjs/operators';
 
 @Component({
     selector: 'dot-edit-page-main',
     templateUrl: './dot-edit-page-main.component.html',
     styleUrls: ['./dot-edit-page-main.component.scss']
 })
-export class DotEditPageMainComponent implements OnInit, OnDestroy {
+export class DotEditPageMainComponent implements OnInit {
     pageState: Observable<DotRenderedPageState>;
-    private routeEventsSub: Subscription;
 
-    constructor(private route: ActivatedRoute, private router: Router) {}
+    constructor(private route: ActivatedRoute) {}
 
-    ngOnInit(): void {
-        this.listenRouteChanges();
-    }
-
-    ngOnDestroy(): void {
-        this.routeEventsSub.unsubscribe();
-    }
-
-    private listenRouteChanges(): void {
-        this.getResolverContent();
-        this.routeEventsSub = this.router.events
-            .pipe(filter((anyEvent: any) => event instanceof NavigationEnd && event.url.indexOf('/edit-page/content') > -1))
-            .subscribe((navigationEvent: NavigationEnd) => {
-                this.getResolverContent();
-            });
-    }
-
-    private getResolverContent(): void {
+    ngOnInit() {
         this.pageState = this.route.data.pluck('content');
     }
 }
