c70387a4427b89203f0b5c34c8fad9fca1a3220f
Freddy Montes <freddy@dotcms.com>
2018-05-24 15:08:13 -0600

    dotCMS/core#13880 Content types: [Violation] 'click' handler took XXXms and auto focus add to bundle dropdown

--- src/app/view/components/_common/action-menu-button/action-menu-button.component.ts
+++ src/app/view/components/_common/action-menu-button/action-menu-button.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { MenuItem } from 'primeng/primeng';
+import { Component, Input, OnInit, ViewChild, ViewContainerRef, ComponentFactoryResolver, ComponentFactory, ComponentRef } from '@angular/core';
+import { MenuItem, Menu } from 'primeng/primeng';
 import { DotDataTableAction } from '../../../../shared/models/data-table/dot-data-table-action';
 
 /**
@@ -14,10 +14,15 @@ import { DotDataTableAction } from '../../../../shared/models/data-table/dot-dat
     templateUrl: 'action-menu-button.component.html'
 })
 export class ActionMenuButtonComponent implements OnInit {
-    filteredActions: MenuItem[] = [];
+    @ViewChild('container', {read: ViewContainerRef}) container: ViewContainerRef;
     @Input() item: any;
     @Input() icon? = 'fa-ellipsis-v';
     @Input() actions?: DotDataTableAction[];
+
+    filteredActions: MenuItem[] = [];
+    private menuComponent: Menu;
+
+    constructor(private componentFactoryResolver: ComponentFactoryResolver) {}
 
     ngOnInit() {
         this.filteredActions = this.actions
@@ -33,5 +38,31 @@ export class ActionMenuButtonComponent implements OnInit {
                     }
                 };
             });
+    }
+
+    /**
+     * Create and inject the menu component on mouse enter;
+     *
+     * @memberof ActionMenuButtonComponent
+     */
+    createMenu(): void {
+        if (!this.menuComponent) {
+            const menu: ComponentFactory<Menu> = this.componentFactoryResolver.resolveComponentFactory(Menu);
+            const menuEl: ComponentRef<Menu> = this.container.createComponent(menu);
+            this.menuComponent = menuEl.instance;
+            this.menuComponent.model = this.filteredActions;
+            this.menuComponent.appendTo = 'body';
+            this.menuComponent.popup = true;
+        }
+    }
+
+    /**
+     * Handle click on the button
+     *
+     * @param {MouseEvent} $event
+     * @memberof ActionMenuButtonComponent
+     */
+    onClick($event: MouseEvent): void {
+        this.menuComponent.toggle($event);
     }
 }
