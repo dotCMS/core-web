ee85f5b4d01ea723173385056dff058236c239aa
Freddy Montes <freddy@dotcms.com>
2018-06-07 07:58:48 -0600

    Update tests

--- src/app/portlets/dot-edit-page/content/components/dot-form-selector/dot-form-selector.component.spec.ts
+++ src/app/portlets/dot-edit-page/content/components/dot-form-selector/dot-form-selector.component.spec.ts
@@ -37,10 +37,11 @@ const messageServiceMock = new MockDotMessageService({
     'dot.common.select': 'Select'
 });
 
-fdescribe('DotFormSelectorComponent', () => {
+describe('DotFormSelectorComponent', () => {
     let component: DotFormSelectorComponent;
     let fixture: ComponentFixture<DotFormSelectorComponent>;
     let de: DebugElement;
+    let paginatorService: PaginatorService;
 
     beforeEach(async(() => {
         DOTTestBed.configureTestingModule({
@@ -53,7 +54,7 @@ fdescribe('DotFormSelectorComponent', () => {
                 {
                     provide: DotMessageService,
                     useValue: messageServiceMock
-                },
+                }
             ],
             imports: [DotDialogModule, BrowserAnimationsModule]
         });
@@ -63,10 +64,28 @@ fdescribe('DotFormSelectorComponent', () => {
         fixture = TestBed.createComponent(DotFormSelectorComponent);
         component = fixture.componentInstance;
         de = fixture.debugElement;
+        paginatorService = fixture.debugElement.injector.get(PaginatorService);
     });
 
-    describe('default', () => {
+    it('should have dot-dialog', () => {
+        expect(de.query(By.css('p-dialog')) !== null).toBe(true);
+    });
+
+    describe('hide dialog', () => {
         beforeEach(() => {
+            fixture.detectChanges();
+        });
+
+        it('should have dot-dialog hidden', () => {
+            const dialog: DebugElement = de.query(By.css('dot-dialog'));
+            expect(dialog.componentInstance.show).toBe(false);
+        });
+    });
+
+    describe('show dialog', () => {
+        beforeEach(() => {
+            spyOn(paginatorService, 'getCurrentPage').and.returnValue(Observable.of([mockContentType]));
+            component.show = true;
             fixture.detectChanges();
         });
 
@@ -82,74 +101,79 @@ fdescribe('DotFormSelectorComponent', () => {
                 expect(pTableComponent).toBeTruthy();
             });
         });
-    });
 
-    describe('data', () => {
-        let pTableComponent: DebugElement;
-        let paginatorService: PaginatorService;
-        const paginationObservable = Observable.of([mockContentType]);
+        describe('data', () => {
+            let pTableComponent: DebugElement;
 
-        beforeEach(() => {
-            pTableComponent = de.query(By.css('p-dataTable'));
-            paginatorService = fixture.debugElement.injector.get(PaginatorService);
+            beforeEach(() => {
 
-            spyOn(paginatorService, 'getCurrentPage').and.returnValue(paginationObservable);
-            fixture.detectChanges();
-        });
-
-        describe('pagination', () => {
-            it('should set the url', () => {
-                expect(paginatorService.url).toBe('v1/contenttype?type=FORM');
+                fixture.detectChanges();
+                pTableComponent = de.query(By.css('p-dataTable'));
             });
 
-            it('should load current page', () => {
-                expect(paginatorService.getCurrentPage).toHaveBeenCalled();
-                expect(pTableComponent.componentInstance.value).toEqual([mockContentType]);
-            });
-        });
+            describe('pagination', () => {
+                it('should set the url', () => {
+                    expect(paginatorService.url).toBe('v1/contenttype?type=FORM');
+                });
 
-        describe('data/columns', () => {
-
-            it('should have two columns and two rows', () => {
-                const headRows = de.queryAll(By.css('table thead tr'));
-                expect(headRows.length).toBe(1, 'must have one row head');
-
-                const firstRowColumns = headRows[0].queryAll(By.css('th'));
-                expect(firstRowColumns.length).toBe(2, 'must have two columns in head');
-
-                const bodyRow = de.queryAll(By.css('table tbody tr'));
-                expect(bodyRow.length).toBe(1, 'must have one row into body');
-
-                const secondRowColumns = bodyRow[0].queryAll(By.css('td'));
-                expect(secondRowColumns.length).toBe(2, 'must have two columns into body');
+                it('should load current page', () => {
+                    expect(paginatorService.getCurrentPage).toHaveBeenCalled();
+                    expect(pTableComponent.componentInstance.value).toEqual([mockContentType]);
+                });
             });
 
-            it('fisrt column should have the right header and content', () => {
-                const label = de.query(By.css('table thead tr:first-child th:first-child span')).nativeElement.innerHTML;
-                expect(label).toBe('Name');
+            describe('data/columns', () => {
+                it('should have two columns and two rows', () => {
+                    const headRows = de.queryAll(By.css('table thead tr'));
+                    expect(headRows.length).toBe(1, 'must have one row head');
 
-                const content = de.query(By.css('table tbody tr:first-child td:first-child span')).nativeElement.innerHTML;
-                expect(content).toBe('Hello World');
+                    const firstRowColumns = headRows[0].queryAll(By.css('th'));
+                    expect(firstRowColumns.length).toBe(2, 'must have two columns in head');
+
+                    const bodyRow = de.queryAll(By.css('table tbody tr'));
+                    expect(bodyRow.length).toBe(1, 'must have one row into body');
+
+                    const secondRowColumns = bodyRow[0].queryAll(By.css('td'));
+                    expect(secondRowColumns.length).toBe(2, 'must have two columns into body');
+                });
+
+                it('first column should have the right header and content', () => {
+                    const label = de.query(By.css('table thead tr:first-child th:first-child span')).nativeElement.innerHTML;
+                    expect(label).toBe('Name');
+
+                    const content = de.query(By.css('table tbody tr:first-child td:first-child span')).nativeElement.innerHTML;
+                    expect(content).toBe('Hello World');
+                });
+
+                it('second column should have the right header and select button', () => {
+                    const label = de.query(By.css('table thead tr:first-child th:nth-child(2n) span')).nativeElement.innerHTML;
+                    expect(label).toBe('');
+
+                    const link = de.query(By.css('.form-selector__button'));
+                    expect(link).not.toBeNull();
+                    expect(link.nativeElement.innerText).toBe('Select');
+                });
             });
 
-            it('second column should have the right header and content', () => {
-                const label = de.query(By.css('table thead tr:first-child th:nth-child(2n) span')).nativeElement.innerHTML;
-                expect(label).toBe('');
+            describe('events', () => {
+                beforeEach(() => {
+                    spyOn(component.select, 'emit');
+                    spyOn(component.close, 'emit');
+                });
 
-                const link = de.query(By.css('table tbody tr:first-child td:nth-child(2n) span a'));
-                expect(link).not.toBeNull();
-                expect(link.nativeElement.innerHTML).toBe('Select');
-            });
-        });
+                it('should emit close', () => {
+                    const dialog: DebugElement = de.query(By.css('dot-dialog'));
+                    dialog.triggerEventHandler('close', {});
 
-        describe('events', () => {
-            it('tigger event when click Select link', () => {
-                spyOn(component.select, 'emit');
+                    expect(component.close.emit).toHaveBeenCalledWith({});
+                });
 
-                const link = de.query(By.css('table tbody tr:first-child td:nth-child(2n) span a'));
-                link.triggerEventHandler('click', null);
+                it('tigger event when click select button', () => {
+                    const button = de.query(By.css('.form-selector__button'));
+                    button.triggerEventHandler('click', null);
 
-                expect(component.select.emit).toHaveBeenCalledWith(mockContentType);
+                    expect(component.select.emit).toHaveBeenCalledWith(mockContentType);
+                });
             });
         });
     });
