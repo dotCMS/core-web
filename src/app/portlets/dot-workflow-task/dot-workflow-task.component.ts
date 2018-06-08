0962809b513a5e1afabcaf536e8a8b5413214986
Alfredo Li <alfredo@dotcms.com>
2018-06-08 17:32:18 -0600

    Add message key

--- src/app/portlets/dot-workflow-task/dot-workflow-task.component.ts
+++ src/app/portlets/dot-workflow-task/dot-workflow-task.component.ts
@@ -1,24 +1,34 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 import { DotWorkflowTaskDetailService } from '../../view/components/dot-workflow-task-detail/services/dot-workflow-task-detail.service';
 import { DotNavigationService } from '../../view/components/dot-navigation/dot-navigation.service';
 import { ActivatedRoute } from '@angular/router';
+import { DotMessageService } from '../../api/services/dot-messages-service';
+import { take } from 'rxjs/operators';
 
 @Component({
     providers: [],
     selector: 'dot-workflow-task',
     template: '<dot-workflow-task-detail (close)="onCloseWorkflowTaskEditor()"></dot-workflow-task-detail>'
 })
-export class DotWorkflowTaskComponent implements AfterViewInit {
+export class DotWorkflowTaskComponent implements AfterViewInit, OnInit {
     constructor(
         private dotWorkflowTaskDetailService: DotWorkflowTaskDetailService,
+        private dotMessageService: DotMessageService,
         private dotNavigationService: DotNavigationService,
         private route: ActivatedRoute
     ) {}
 
+    ngOnInit(): void {
+        this.dotMessageService
+            .getMessages(['workflow.task.dialog.header'])
+            .pipe(take(1))
+            .subscribe();
+    }
+
     ngAfterViewInit(): void {
         setTimeout(() => {
             this.dotWorkflowTaskDetailService.view({
-                header: 'Task Detail',
+                header: this.dotMessageService.get('workflow.task.dialog.header'),
                 id: this.route.snapshot.params.id
             });
         }, 0);
