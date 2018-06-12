c6b85859b8c713362a583bb29b8395cb716aebfb
Alfredo Li <alfredo@dotcms.com>
2018-06-12 10:39:05 -0600

    Add onCloseDialog event

--- src/app/portlets/dot-edit-page/main/dot-edit-page-main/dot-edit-page-main.component.ts
+++ src/app/portlets/dot-edit-page/main/dot-edit-page-main/dot-edit-page-main.component.ts
@@ -1,28 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
 import { DotRenderedPageState } from '../../shared/models/dot-rendered-page-state.model';
 import { DotContentletEditorService } from '../../../../view/components/dot-contentlet-editor/services/dot-contentlet-editor.service';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
     selector: 'dot-edit-page-main',
     templateUrl: './dot-edit-page-main.component.html',
     styleUrls: ['./dot-edit-page-main.component.scss']
 })
-export class DotEditPageMainComponent implements OnInit {
+export class DotEditPageMainComponent implements OnInit, OnDestroy {
     pageState: Observable<DotRenderedPageState>;
+    private destroy$: Subject<boolean> = new Subject<boolean>();
 
     constructor(private route: ActivatedRoute, private dotContentletEditorService: DotContentletEditorService) {}
 
     ngOnInit() {
         this.pageState = this.route.data.pluck('content');
+        this.subscribeIframeCloseAction();
     }
 
-    openProperties(buttonClicked: { inode: string; label: string }) {
+    ngOnDestroy(): void {
+        this.destroy$.next(true);
+        this.destroy$.complete();
+    }
+
+    /**
+     * Handle the call of contentlet dialog
+     *
+     * @param {inode: string; label: string} buttonClicked
+     * @memberof DotEditPageMainComponent
+     */
+    openPageProperties(buttonClicked: { inode: string; label: string }) {
         this.dotContentletEditorService.edit({
             data: {
                 inode: buttonClicked.inode
             }
+        });
+    }
+
+    private subscribeIframeCloseAction(): void {
+        this.dotContentletEditorService.close$.pipe(takeUntil(this.destroy$)).subscribe((closedEvent: boolean) => {
+            console.log('---event close', closedEvent);
         });
     }
 }
