d14c783ee7ee42c65c7244d4acb8c1f578215c44
Freddy Montes <freddy@dotcms.com>
2018-06-16 08:17:00 -0600

    Use pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NotLicensedService } from './api/services/not-licensed-service';
 import { DotcmsConfig } from 'dotcms-js/dotcms-js';
 import { DotUiColors, DotUiColorsService } from './api/services/dot-ui-colors/dot-ui-colors.service';
+import { take, pluck } from 'rxjs/operators';
 
 @Component({
     selector: 'dot-root',
@@ -17,9 +18,14 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.dotCmsConfig.getConfig().take(1).pluck('colors').subscribe((colors: DotUiColors) => {
-            this.dotUiColors.setColors(colors);
-        });
+        this.dotCmsConfig
+            .getConfig()
+            .pipe(
+                take(1),
+                pluck('colors')
+            )
+            .subscribe((colors: DotUiColors) => {
+                this.dotUiColors.setColors(colors);
+            });
     }
-
 }
