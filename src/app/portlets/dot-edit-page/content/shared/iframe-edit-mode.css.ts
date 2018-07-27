0bf759dc13fd766555d413af9a766446905e2517
Alfredo Li <alfredo@dotcms.com>
2018-07-27 15:53:58 -0600

    Hide empty container

--- src/app/portlets/dot-edit-page/content/shared/iframe-edit-mode.css.ts
+++ src/app/portlets/dot-edit-page/content/shared/iframe-edit-mode.css.ts
@@ -43,8 +43,16 @@ export const getEditPageCss = (timestampId: string): string => {
         transition: background ${animation} !important;
     }
 
+    ${timestampId} [data-dot-object="contentlet"][data-dot-has-page-lang-version="false"] {
+        background: none !important;
+    }
+
     ${timestampId} [data-dot-object="container"]:hover [data-dot-object="contentlet"] {
         background: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAQElEQVQoU2NkIAIEH/r5n5GQOpCitXbsjHgVwhSBDMOpEFkRToXoirAqxKYIQyEuRSgK8SmCKySkCKyQGEUghQCQPycYlScX0wAAAABJRU5ErkJggg==") !important;
+    }
+
+    ${timestampId} [data-dot-object="container"]:hover [data-dot-object="contentlet"][data-dot-has-page-lang-version="false"] {
+        background: none !important;
     }
 
     ${timestampId} div[data-dot-object="edit-content"] {
@@ -184,6 +192,10 @@ export const getEditPageCss = (timestampId: string): string => {
 
     ${timestampId} [data-dot-object="contentlet"]:hover .dotedit-contentlet__toolbar {
         opacity: 1 !important;
+    }
+
+    ${timestampId} [data-dot-object="contentlet"][data-dot-has-page-lang-version="false"]:hover .dotedit-contentlet__toolbar  {
+        opacity: 0 !important;
     }
 
     ${timestampId} .dotedit-contentlet__toolbar button {
