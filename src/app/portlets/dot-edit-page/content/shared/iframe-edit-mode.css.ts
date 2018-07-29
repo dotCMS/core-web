ae712d519617ac32f389ab829e91829d5e67eba8
Freddy Montes <freddy@dotcms.com>
2018-07-29 07:40:57 -0600

    Hide div with no content, fix the reorder save event

--- src/app/portlets/dot-edit-page/content/shared/iframe-edit-mode.css.ts
+++ src/app/portlets/dot-edit-page/content/shared/iframe-edit-mode.css.ts
@@ -44,15 +44,11 @@ export const getEditPageCss = (timestampId: string): string => {
     }
 
     ${timestampId} [data-dot-object="contentlet"][data-dot-has-page-lang-version="false"] {
-        background: none !important;
+        display: none !important;
     }
 
     ${timestampId} [data-dot-object="container"]:hover [data-dot-object="contentlet"] {
         background: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAQElEQVQoU2NkIAIEH/r5n5GQOpCitXbsjHgVwhSBDMOpEFkRToXoirAqxKYIQyEuRSgK8SmCKySkCKyQGEUghQCQPycYlScX0wAAAABJRU5ErkJggg==") !important;
-    }
-
-    ${timestampId} [data-dot-object="container"]:hover [data-dot-object="contentlet"][data-dot-has-page-lang-version="false"] {
-        background: none !important;
     }
 
     ${timestampId} div[data-dot-object="edit-content"] {
