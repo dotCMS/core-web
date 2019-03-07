b86c81409b211238c5b8bc7c40356d14a4f6bf12
Alfredo Li <alfredo@dotcms.com>
2019-03-07 11:08:13 -0600

    add 'dot-icon.component' with tradebacks

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,27 @@
 const path = require('path');
 
-// Export a function. Accept the base config as the only param.
-module.exports = ({ config, mode }) => {
-  // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-  // You can change the configuration based on that.
-  // 'PRODUCTION' is used when building the static version of storybook.
+// // Export a function. Accept the base config as the only param.
+// module.exports = ({ config, mode }) => {
+//   // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+//   // You can change the configuration based on that.
+//   // 'PRODUCTION' is used when building the static version of storybook.
 
-  // Make whatever fine-grained changes you need
-  config.module.rules.push({
-    test: /\.scss$/,
-    loaders: ['style-loader', 'css-loader', 'sass-loader'],
-    include: path.resolve(__dirname, '../'),
-  });
+//   // Make whatever fine-grained changes you need
+//   config.module.rules.push({
+//     test: /\.scss$/,
+//     loaders: ['style-loader', 'css-loader', 'sass-loader'],
+//     include: path.resolve(__dirname, '../'),
+//   });
 
-  // Return the altered config
-  return config;
+//   // Return the altered config
+//   return config;
+// };
+
+
+module.exports = {
+	resolve: {
+		alias: {
+			styles: path.resolve(__dirname, '../src/styles')
+		}
+	}
 };
