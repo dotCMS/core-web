b17f432f3537c6331a395c8e50e64e595f84a8b1
Humberto Morera <humberto.morera@dotcms.com>
2019-03-15 07:48:02 -0600

    login container test update

--- src/app/view/components/login/dot-login-component/login-container.component.spec.ts
+++ src/app/view/components/login/dot-login-component/login-container.component.spec.ts
@@ -2,18 +2,32 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DotLoadingIndicatorService } from '../../_common/iframe/dot-loading-indicator/dot-loading-indicator.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { DotRouterService } from '@services/dot-router/dot-router.service';
-import { DotLoadingIndicatorComponent } from '../../_common/iframe/dot-loading-indicator/dot-loading-indicator.component';
 import { MdInputTextModule } from '../../../directives/md-inputtext/md-input-text.module';
 import { LoginContainerComponent } from './login-container.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { async } from '@angular/core/testing';
-import { DotLoginComponent } from '@components/login/dot-login-component/dot-login.component';
 import { By } from '@angular/platform-browser';
 import { DOTTestBed } from '../../../../test/dot-test-bed';
 import { LoginService } from 'dotcms-js';
 import { LoginServiceMock } from '../../../../test/login-service.mock';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {DotLoginData} from '@models/dot-login';
 
-describe('LoginContainerComponent', () => {
+@Component({
+    selector: 'dot-login-component',
+    template: ''
+})
+class TestDotLoginComponent {
+    @Input() isLoginInProgress = false;
+    @Input() message = '';
+    @Input() passwordChanged = false;
+    @Input() resetEmailSent = false;
+    @Input() resetEmail = '';
+    @Output() recoverPassword = new EventEmitter<any>();
+    @Output() login = new EventEmitter<DotLoginData>();
+}
+
+fdescribe('LoginContainerComponent', () => {
     let component: LoginContainerComponent;
     let loginService: LoginService;
     let dotRouterService: DotRouterService;
@@ -22,7 +36,7 @@ describe('LoginContainerComponent', () => {
     beforeEach(async(() => {
         DOTTestBed.configureTestingModule({
             imports: [MdInputTextModule, RouterTestingModule, BrowserAnimationsModule],
-            declarations: [LoginContainerComponent, DotLoginComponent, DotLoadingIndicatorComponent],
+            declarations: [LoginContainerComponent, TestDotLoginComponent],
             providers: [
                 DotLoadingIndicatorService,
                 { provide: LoginService, useClass: LoginServiceMock }
@@ -47,7 +61,7 @@ describe('LoginContainerComponent', () => {
             spyOn(loginService, 'loginUser').and.callThrough();
             spyOn(component, 'logInUser').and.callThrough();
             spyOn(dotRouterService, 'goToMain');
-            const loginComponent: DotLoginComponent = fixture.debugElement.query(
+            const loginComponent: TestDotLoginComponent = fixture.debugElement.query(
                 By.css('dot-login-component')
             ).componentInstance;
 
