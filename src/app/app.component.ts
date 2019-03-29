bcd22163b433e9add58b1f1a36d4bd141842b5cb
Alfredo Li <alfredo@dotcms.com>
2019-03-29 08:59:36 -0600

    Clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { NotLicensedService } from '@services/not-licensed-service';
 import { DotcmsConfig } from 'dotcms-js';
 import { DotUiColors, DotUiColorsService } from '@services/dot-ui-colors/dot-ui-colors.service';
 import { take, pluck } from 'rxjs/operators';
-import { initDotCMS } from '../../projects/dotcms/src/public_api';
 
 @Component({
     selector: 'dot-root',
@@ -20,23 +19,6 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit() {
-        const dotcms = initDotCMS({
-            token:
-                // tslint:disable-next-line:max-line-length
-                'eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlYWJmNDc1MS1iZDQ3LTRhYWEtODJhYy1kZmU5MjAzOGQ4NGUiLCJ4bW9kIjoxMjA0ODI0OTYxMDAwLCJzdWIiOiJkb3RjbXMub3JnLjEiLCJpYXQiOjE1NTM3ODY5NTEsImlzcyI6ImE3MTVlYzAwLTRkZmUtNDUwNC1hZDgwLTRkNDczMTMwZDNlNiIsImV4cCI6MTU1Mzg3MzM1MX0.NYW9AD1-2oBL6zmuBypF5DAOOFbUI8vgkMGDP8RV-Io'
-        });
-
-        const param = {
-            title: `titulo${new Date().getTime()}`,
-            body: 'valor',
-            contentHost: 'demo.dotcms.com',
-            stName: 'webPageContent'
-        };
-
-        dotcms.contentType.save(param).then((data) => {
-            console.log('---data', data);
-        });
-
         this.dotCmsConfig
             .getConfig()
             .pipe(
