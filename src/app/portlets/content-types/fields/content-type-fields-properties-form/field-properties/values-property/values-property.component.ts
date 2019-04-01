7239014f5db23a8c2dfd15fd54de1fcb7c4f072d
Humberto Morera <humberto.morera@dotcms.com>
2019-04-01 15:56:02 -0600

    progress

--- src/app/portlets/content-types/fields/content-type-fields-properties-form/field-properties/values-property/values-property.component.ts
+++ src/app/portlets/content-types/fields/content-type-fields-properties-form/field-properties/values-property/values-property.component.ts
@@ -7,26 +7,39 @@ import { take } from 'rxjs/operators';
 
 @Component({
     selector: 'dot-values-property',
-    templateUrl: './values-property.component.html'
+    templateUrl: './values-property.component.html',
+    styleUrls: ['./values-property.component.scss']
 })
 export class ValuesPropertyComponent implements OnInit {
-    @ViewChild('value')
-    value: DotTextareaContentComponent;
+    @ViewChild('value') value: DotTextareaContentComponent;
     property: FieldProperty;
     group: FormGroup;
+    helpText: string;
 
     i18nMessages: {
         [key: string]: string;
     } = {};
 
+    private validTextHelperClazz = [
+        'com.dotcms.contenttype.model.field.ImmutableRadioField',
+        'com.dotcms.contenttype.model.field.ImmutableCheckboxField',
+        'com.dotcms.contenttype.model.field.ImmutableMultiSelectField',
+        'com.dotcms.contenttype.model.field.ImmutableSelectField'
+    ];
+
     constructor(public dotMessageService: DotMessageService) {}
 
     ngOnInit() {
+        console.log(this.validTextHelperClazz);
         this.dotMessageService
             .getMessages(['contenttypes.field.properties.value.label'])
             .pipe(take(1))
-            .subscribe((res) => {
+            .subscribe(res => {
                 this.i18nMessages = res;
             });
+    }
+
+    isValidHelperClass(): boolean {
+        return this.validTextHelperClazz.includes(this.property.field.clazz);
     }
 }
