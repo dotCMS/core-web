1d044439fb947660e6e41304be30549e39478e76
Freddy Montes <freddy@dotcms.com>
2019-05-17 12:05:14 -0600

    Fix types

--- projects/dotcms-field-elements/src/components/dot-select/dot-select.tsx
+++ projects/dotcms-field-elements/src/components/dot-select/dot-select.tsx
@@ -53,7 +53,7 @@ export class DotSelectComponent {
 
     @Watch('options')
     optionsWatch(): void {
-        const validOptions = dotPropValidator<DotSelectComponent, DotOption[]>(this, 'options');
+        const validOptions = dotPropValidator<DotSelectComponent, string>(this, 'options');
         this._options = getDotOptionsFromFieldValue(validOptions);
     }
 
