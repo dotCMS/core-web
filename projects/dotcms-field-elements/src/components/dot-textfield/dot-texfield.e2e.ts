ae101deb14c876c843ec47fad718c4b856059497
Humberto Morera <humberto.morera@dotcms.com>
2019-05-21 11:01:42 -0600

    initla progress

--- projects/dotcms-field-elements/src/components/dot-textfield/dot-texfield.e2e.ts
+++ projects/dotcms-field-elements/src/components/dot-textfield/dot-texfield.e2e.ts
@@ -1,5 +1,6 @@
 import { E2EElement, E2EPage, newE2EPage } from '@stencil/core/testing';
 import { EventSpy } from '@stencil/core/dist/declarations';
+import { Prop } from '@stencil/core';
 
 describe('dot-textfield', () => {
     let page: E2EPage;
@@ -8,65 +9,154 @@ describe('dot-textfield', () => {
     let spyStatusChangeEvent: EventSpy;
     let spyValueChange: EventSpy;
 
-    beforeEach(async () => {
-        page = await newE2EPage({
-            html: `
-              <dot-textfield
-                    label='Name:'
-                    name='fullName'
-                    value='John'
-                    hint='this is a hint'
-                    placeholder='Enter Name'
-                    regex-check='^[A-Za-z ]+$'
-                    validation-message="Invalid Name"
-                    required
-                    required-message="Required Name"
-                ></dot-textfield>`
+    const getLabel = async () => await page.find('label');
+
+    //     /** Value specifies the value of the <input> element */
+    // @Prop() name
+    // @prop() label =
+    // @Prop() placeholder
+    // @Prop() hint
+    // @Prop() required
+    // @Prop() requiredMessage
+    // @Prop() validationMessage
+    // @Prop() disabled = false;
+    // @Prop regexCheck = '';
+    // @Prop() type = 'text';
+
+
+    // beforeEach(async () => {
+    //     page = await newE2EPage({
+    //         html: `<dot-textfield></dot-textfield>`
+    //     });
+    //
+    //     spyStatusChangeEvent = await page.spyOnEvent('statusChange');
+    //     spyValueChange = await page.spyOnEvent('valueChange');
+    //     element = await page.find('dot-textfield');
+    //     input = await page.find('input');
+    // });
+
+    describe('@Props', () => {
+
+        beforeEach(async () => {
+            page = await newE2EPage({
+                html: `<dot-textfield></dot-textfield>`
+            });
+
+            element = await page.find('dot-textfield');
+            input = await page.find('input');
         });
 
-        spyStatusChangeEvent = await page.spyOnEvent('statusChange');
-        spyValueChange = await page.spyOnEvent('valueChange');
-        element = await page.find('dot-textfield');
-        input = await page.find('input');
+
+        describe('value', () => {
+            it('should set value correctly', async() => {
+                element.setProperty('value', 'test');
+                await page.waitForChanges();
+
+                expect(await input.getProperty('value')).toBe('test');
+            });
+            it('should set object string value correctly', async() => {
+                element.setProperty('value', {test: true});
+                await page.waitForChanges();
+
+                expect(await input.getProperty('value')).toBe('[object Object]');
+            });
+        });
+
+        describe('name', () => {
+
+            it('should render with valid value', async () => {
+                element.setProperty('name', 'someCamelCas*eNa&me&$');
+                await page.waitForChanges();
+                const label = await getLabel();
+                expect(await label.getAttribute('id')).toBe('label-somecamelcasename');
+            });
+
+            it('should not render when not defined', async () => {
+                element.setProperty('name', undefined);
+                await page.waitForChanges();
+                const label = await getLabel();
+                expect(await label.getAttribute('id')).toBe(null);
+            });
+
+            it('should not render with invalid value', async () => {
+                element.setProperty('name', null);
+                await page.waitForChanges();
+                const label = await getLabel();
+                expect(await label.getProperty('id')).toBe('');
+            });
+        });
+
+        // describe('propety name 2', () => {
+        //     xit('') {}
+        //
+        // })
     });
 
-    it('should render', () => {
+    // describe('@Events', () => {
+    //     describe('status and value change', () => {
+    //         xit('') {}
+    //         xit('') {}
+    //         xit('') {}
+    //     })
+    //
+    //     describe('status change', () => {
+    //         xit('') {}
+    //     })
+    //
+    //     describe('value change', () => {
+    //         xit('') {}
+    //     })
+    // })
+    //
+    // describe('@Method', () => {
+    //     describe('method name', () => {
+    //         xit('') {}
+    //         xit('') {}
+    //         xit('') {}
+    //     })
+    //
+    //     describe('method name 2', () => {
+    //         xit('') {}
+    //     })
+    // })
+
+    /*xit('should render', () => {
         // tslint:disable-next-line:max-line-length
         const tagsRenderExpected = `<div class=\"dot-field__label\"><label for=\"dot-fullName\">Name:</label><span class=\"dot-field__required-mark\">*</span></div><input id=\"dot-fullName\" placeholder=\"Enter Name\" required=\"\" type=\"text\"><span class=\"dot-field__hint\">this is a hint</span>`;
         expect(element.innerHTML).toBe(tagsRenderExpected);
     });
 
-    it('should set type', async () => {
+    xit('should set type', async () => {
         element.setProperty('type', 'email');
         await page.waitForChanges();
         expect(await input.getProperty('type')).toBe('email');
     });
 
-    it('should show Regex validation message', async () => {
+    xit('should show Regex validation message', async () => {
         await input.press('@');
         await page.waitForChanges();
         const errorMessage = await page.find('.dot-field__error-message');
         expect(errorMessage.innerHTML).toBe('Invalid Name');
     });
 
-    it('should load as pristine and untouched', () => {
+    xit('should load as pristine and untouched', () => {
         expect(element.classList.contains('dot-pristine')).toBe(true);
         expect(element.classList.contains('dot-untouched')).toBe(true);
     });
 
-    it('should mark as dirty and touched when type', async () => {
+    xit('should mark as dirty and touched when type', async () => {
         await input.press('a');
         await page.waitForChanges();
         expect(element).toHaveClasses(['dot-dirty', 'dot-touched']);
     });
 
-    it('should mark as invalid when value dont match REgex', async () => {
+    xit('should mark as invalid when value dont match REgex', async () => {
         await input.press('@');
         await page.waitForChanges();
         expect(element).toHaveClasses(['dot-invalid']);
     });
 
-    it('should clear value, set pristine and untouched  when input set reset', async () => {
+    xit('should clear value, set pristine and untouched  when input set reset', async () => {
         await element.callMethod('reset');
         await page.waitForChanges();
 
@@ -76,24 +166,24 @@ describe('dot-textfield', () => {
         expect(await input.getProperty('value')).toBe('');
     });
 
-    it('should mark as disabled when prop is present', async () => {
+    xit('should mark as disabled when prop is present', async () => {
         element.setProperty('disabled', true);
         await page.waitForChanges();
         expect(await input.getProperty('disabled')).toBe(true);
     });
 
-    it('should mark as required when prop is present', async () => {
+    xit('should mark as required when prop is present', async () => {
         expect(await input.getProperty('required')).toBe(true);
     });
 
-    it('should set the default value of regexCheck when the Regular Expression is not valid', async () => {
+    xit('should set the default value of regexCheck when the Regular Expression is not valid', async () => {
         element.setProperty('regexCheck', '[^(<[.\\n]+>)]*l');
         await page.waitForChanges();
         expect(await input.getProperty('regexCheck')).toBeUndefined();
     });
 
     describe('emit events', () => {
-        it('should mark as touched when onblur', async () => {
+        xit('should mark as touched when onblur', async () => {
             await input.triggerEvent('blur');
             await page.waitForChanges();
 
@@ -107,7 +197,7 @@ describe('dot-textfield', () => {
             });
         });
 
-        it('should send status and value change', async () => {
+        xit('should send status and value change', async () => {
             await input.press('a');
             await page.waitForChanges();
             expect(spyStatusChangeEvent).toHaveReceivedEventDetail({
@@ -124,7 +214,7 @@ describe('dot-textfield', () => {
             });
         });
 
-        it('should emit status and value on Reset', async () => {
+        xit('should emit status and value on Reset', async () => {
             await element.callMethod('reset');
             expect(spyStatusChangeEvent).toHaveReceivedEventDetail({
                 name: 'fullName',
@@ -136,5 +226,5 @@ describe('dot-textfield', () => {
             });
             expect(spyValueChange).toHaveReceivedEventDetail({ name: 'fullName', value: '' });
         });
-    });
+    });*/
 });
