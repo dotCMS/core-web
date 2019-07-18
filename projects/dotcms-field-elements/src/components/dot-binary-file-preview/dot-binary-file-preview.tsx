142a067f36ac2b9ebf5edc16cb1336ded656c433
Humberto Morera <humberto.morera@dotcms.com>
2019-07-18 14:14:38 -0600

    progress

---
+++ projects/dotcms-field-elements/src/components/dot-binary-file-preview/dot-binary-file-preview.tsx
@@ -0,0 +1,57 @@
+import { Component, Element, Event, EventEmitter, Prop } from '@stencil/core';
+import Fragment from 'stencil-fragment';
+import { DotCMSTempFile } from '../../models';
+
+@Component({
+    tag: 'dot-binary-file-preview',
+    styleUrl: 'dot-binary-file-preview.scss'
+})
+export class DotBinaryFilePreviewComponent {
+    @Element() el: HTMLElement;
+
+    /** file to be displayed */
+    @Prop({ reflectToAttr: true, mutable: true })
+    file: DotCMSTempFile = null;
+
+    /** (optional) Delete button's label */
+    @Prop({ reflectToAttr: true })
+    deleteLabel = 'Delete';
+
+    /** Emit when the  */
+    @Event() delete: EventEmitter;
+
+    render() {
+        return this.file ? (
+            <Fragment>
+                {this.getPreviewElement()}
+                <div class="dot-file-preview__info">
+                    <span class="dot-file-preview__name">
+                        {this.file ? this.file.fileName : ''}
+                    </span>
+                    <button onClick={() => this.clearFile()}>{this.deleteLabel}</button>
+                </div>
+            </Fragment>
+        ) : null;
+    }
+
+    private clearFile(): void {
+        this.delete.emit();
+        this.file = null;
+    }
+
+    private getPreviewElement(): JSX.Element {
+        return this.file ? (
+            this.file.image ? (
+                <img src={this.file.thumbnailUrl} />
+            ) : (
+                <div class="dot-file-preview__extention">
+                    <span>{this.getExtention()}</span>
+                </div>
+            )
+        ) : null;
+    }
+
+    private getExtention(): string {
+        return this.file.fileName.substr(this.file.fileName.indexOf('.'));
+    }
+}
---
+++ projects/dotcms-field-elements/src/components/dot-binary-file-preview/readme.md
@@ -0,0 +1,25 @@
+# dot-binary-file-preview
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property      | Attribute      | Description                      | Type             | Default    |
+| ------------- | -------------- | -------------------------------- | ---------------- | ---------- |
+| `deleteLabel` | `delete-label` | (optional) Delete button's label | `string`         | `'Delete'` |
+| `file`        | --             | file to be displayed             | `DotCMSTempFile` | `null`     |
+
+
+## Events
+
+| Event    | Description   | Type                |
+| -------- | ------------- | ------------------- |
+| `delete` | Emit when the | `CustomEvent<void>` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
