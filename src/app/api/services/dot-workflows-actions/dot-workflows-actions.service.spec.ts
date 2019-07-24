9fb6dcf659b93cb12df3730f06846862c690853d
Freddy Montes <freddy@dotcms.com>
2019-07-24 13:59:24 -0600

    Update DotWorkflowsActionsService tests

--- src/app/api/services/dot-workflows-actions/dot-workflows-actions.service.spec.ts
+++ src/app/api/services/dot-workflows-actions/dot-workflows-actions.service.spec.ts
@@ -1,12 +1,45 @@
-import { TestBed } from '@angular/core/testing';
+import { ConnectionBackend, ResponseOptions, Response } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 
 import { DotWorkflowsActionsService } from './dot-workflows-actions.service';
+import { DOTTestBed } from '@tests/dot-test-bed';
+import { mockWorkflowsActions } from '@tests/dot-workflows-actions.mock';
+import { DotWorkflowAction } from '@shared/models/dot-workflow-action/dot-workflow-action.model';
 
 describe('DotWorkflowsActionsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    let TestBed;
+    let dotWorkflowService;
+    let backend;
+    let lastConnection;
 
-  it('should be created', () => {
-    const service: DotWorkflowsActionsService = TestBed.get(DotWorkflowsActionsService);
-    expect(service).toBeTruthy();
-  });
+    beforeEach(() => {
+        TestBed = DOTTestBed.resolveAndCreate([DotWorkflowsActionsService]);
+        dotWorkflowService = TestBed.get(DotWorkflowsActionsService);
+        backend = TestBed.get(ConnectionBackend) as MockBackend;
+        backend.connections.subscribe((connection: any) => {
+            lastConnection = connection;
+        });
+    });
+
+    it('should get actions', () => {
+        let result;
+        dotWorkflowService.get(['123', '456']).subscribe((res: DotWorkflowAction[]) => {
+            result = res;
+        });
+
+        lastConnection.mockRespond(
+            new Response(
+                new ResponseOptions({
+                    body: {
+                        entity: [...mockWorkflowsActions]
+                    }
+                })
+            )
+        );
+
+        expect(lastConnection.request.method).toBe(1);
+        expect(lastConnection.request.url).toBe('/api/v1/workflow/schemes/actions/NEW');
+        expect(lastConnection.request._body).toEqual({ schemes: ['123', '456'] });
+        expect(result).toEqual([...mockWorkflowsActions]);
+    });
 });
