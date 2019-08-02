513a59a15263d8e514de2be0d78ed8730c6f54ad
Humberto Morera <humberto.morera@dotcms.com>
2019-08-02 11:38:36 -0600

    dot-spinner + test

--- src/app/view/components/_common/dot-global-message/dot-global-message.module.ts
+++ src/app/view/components/_common/dot-global-message/dot-global-message.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { DotGlobalMessageService } from './dot-global-message.service';
 import { DotGlobalMessageComponent } from './dot-global-message.component';
 import { DotIconModule } from '@components/_common/dot-icon/dot-icon.module';
+import { DotSpinnerModule } from '@components/_common/dot-spinner/dot-spinner.module';
 
 @NgModule({
-    imports: [CommonModule, DotIconModule],
+    imports: [CommonModule, DotIconModule, DotSpinnerModule],
     declarations: [DotGlobalMessageComponent],
     exports: [DotGlobalMessageComponent],
     providers: [DotGlobalMessageService]
