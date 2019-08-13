7687cf7d11b131ba5fae10d72a67152f7b8d60fe
Freddy Rodriguez <freddyrodriguez@Freddys-MacBook-Pro.local>
2019-08-13 13:29:38 -0600

    Update the persona selector label / fix message service error

--- src/app/view/components/dot-persona-selected-item/dot-persona-selected-item.component.ts
+++ src/app/view/components/dot-persona-selected-item/dot-persona-selected-item.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, Output, EventEmitter, HostListener, OnInit } from '@a
 import { DotPersona } from '@models/dot-persona/dot-persona.model';
 import { DotMessageService } from '@services/dot-messages-service';
 import { take } from 'rxjs/operators';
+import { DotPageStateService } from '@portlets/dot-edit-page/content/services/dot-page-state/dot-page-state.service';
+import { DotPageMode, DotRenderedPageState } from '@portlets/dot-edit-page/shared/models';
 
 @Component({
     selector: 'dot-persona-selected-item',
@@ -15,17 +17,29 @@ export class DotPersonaSelectedItemComponent implements OnInit {
     @Output()
     selected = new EventEmitter<MouseEvent>();
 
+    isEditMode = false;
     messages: { [key: string]: string } = {};
 
-    constructor(private dotMessageService: DotMessageService) {}
+    constructor(
+        private dotMessageService: DotMessageService,
+        private dotPageStateService: DotPageStateService) {}
 
     ngOnInit() {
         this.dotMessageService
-            .getMessages(['modes.persona.selector.title', 'modes.persona.no.persona'])
+            .getMessages([
+                'modes.persona.selector.title.preview',
+                'modes.persona.selector.title.edit',
+                'modes.persona.no.persona'
+            ])
             .pipe(take(1))
             .subscribe((messages: { [key: string]: string }) => {
                 this.messages = messages;
+                console.log('this.messages', this.messages);
             });
+
+        this.dotPageStateService.state$.subscribe((dotRenderedPageState: DotRenderedPageState) => {
+            this.isEditMode = dotRenderedPageState.state.mode === DotPageMode.EDIT;
+        });
     }
 
     @HostListener('click', ['$event'])
