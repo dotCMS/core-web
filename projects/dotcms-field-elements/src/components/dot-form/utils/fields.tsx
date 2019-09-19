c9afdb586b3737bd263b8289443a713bd5026a0f
Alfredo Li <alfredo@dotcms.com>
2019-09-19 13:24:00 -0600

    fix

--- projects/dotcms-field-elements/src/components/dot-form/utils/fields.tsx
+++ projects/dotcms-field-elements/src/components/dot-form/utils/fields.tsx
@@ -157,6 +157,9 @@ export const DotFormFields = {
             hint={field.hint}
             label={field.name}
             name={field.variable}
+            ref={(el: HTMLElement) => {
+                setAttributesToTag(el, field.fieldVariables);
+            }}
             required={field.required}
         />
     )
