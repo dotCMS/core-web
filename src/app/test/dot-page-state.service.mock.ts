b26caff4b7d6f3d84c63d8de996302ff600bc226
Humberto Morera <humberto.morera@dotcms.com>
2019-10-15 10:03:03 -0500

    add methods used by dot-edit-page-view-as-controller.component

--- src/app/test/dot-page-state.service.mock.ts
+++ src/app/test/dot-page-state.service.mock.ts
@@ -4,6 +4,8 @@ import { of, Observable, BehaviorSubject } from 'rxjs';
 import { DotPageRenderState } from '@portlets/dot-edit-page/shared/models/dot-rendered-page-state.model';
 import { mockDotRenderedPageState } from './dot-rendered-page-state.mock';
 import { DotPageRenderOptions } from '@services/dot-page-render/dot-page-render.service';
+import { DotDevice } from '@models/dot-device/dot-device.model';
+import { DotPersona } from '@models/dot-persona/dot-persona.model';
 
 @Injectable()
 export class DotPageStateServiceMock {
@@ -14,4 +16,10 @@ export class DotPageStateServiceMock {
     }
 
     setLock(_options: DotPageRenderOptions, _lock: boolean = null): void {}
+
+    setDevice(_device: DotDevice): void {}
+
+    setLanguage(_language: number): void {}
+
+    setPersona(_persona: DotPersona): void {}
 }
