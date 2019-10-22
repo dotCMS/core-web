89db983a6d5d66419099505ddf5626bad3d0e01a
Freddy Rodriguez <freddyrodriguez@Freddys-MacBook-Pro.local>
2019-10-22 08:35:02 -0600

    using ChromeHeadlessNoSandbox

--- karma.conf.js
+++ karma.conf.js
@@ -25,9 +25,9 @@ module.exports = function(config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['ChromeHeadlessCI'],
+        browsers: ['ChromeHeadlessNoSandbox'],
         customLaunchers: {
-            ChromeHeadlessCI: {
+            ChromeHeadlessNoSandbox: {
                 base: 'ChromeHeadless',
                 flags: ['--no-sandbox']
             }
