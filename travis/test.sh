678b8a52b76b31e048261582f49ffdeee5165c61
Freddy Rodriguez <freddyrodriguez@Freddys-MacBook-Pro.local>
2019-11-18 07:12:37 -0600

    pass current_branch env

--- travis/test.sh
+++ travis/test.sh
@@ -3,6 +3,12 @@
 bash travis/printStoragePaths.sh
 ignoring_return_value=$?
 
+CURRENT_BRANCH=$TRAVIS_PULL_REQUEST_BRANCH
+if [ "$TRAVIS_PULL_REQUEST" = "false" ];
+then
+  CURRENT_BRANCH=$TRAVIS_BRANCH
+fi
+
 # Using this approach in order to avoid travis timeouts waithing for log movement
 
 function bell() {
@@ -15,5 +21,5 @@ bell &
 
 gcloud builds submit \
   --config=travis/cloudrun-test.yml \
-  --substitutions=_CUSTOM_RUN_ID=$TRAVIS_COMMIT_SHORT,_CURRENT_BRANCH=$TRAVIS_BRANCH,_COMMIT_SHORT=$TRAVIS_COMMIT_SHORT,_GOOGLE_CREDENTIALS_BASE64=$GOOGLE_CREDENTIALS_BASE64 .
+  --substitutions=_CUSTOM_RUN_ID=$TRAVIS_COMMIT_SHORT,_CURRENT_BRANCH=$CURRENT_BRANCH,_COMMIT_SHORT=$TRAVIS_COMMIT_SHORT,_GOOGLE_CREDENTIALS_BASE64=$GOOGLE_CREDENTIALS_BASE64 .
 exit $?
