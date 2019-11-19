7b81890e8612317ac39fb7cbed5d777760aa287d
Freddy Rodriguez <freddyrodriguez@Freddys-MacBook-Pro.local>
2019-11-19 08:22:27 -0600

    updating github status

--- docker/run_and_storage.sh
+++ docker/run_and_storage.sh
@@ -2,7 +2,8 @@ echo 'Running test'
 npm test -- dotcms-ui --watch=false --reporters=html,progress
 npmTestExitValue=$?
 
-if [ "${GOOGLE_CREDENTIALS_BASE64}" ] then
+if [ "${GOOGLE_CREDENTIALS_BASE64}" ];
+ then
     echo 'Running storage'
     bash storage.sh
     exit $npmTestExitValue
