ef750e0e49237bc7998569142f13f2d74b6c4893
Alfredo Li <alfredo@dotcms.com>
2019-11-25 16:44:17 -0600

    Progress

--- projects/dotcms/src/lib/api/DotApiContent.ts
+++ projects/dotcms/src/lib/api/DotApiContent.ts
@@ -1,5 +1,22 @@
 import { DotCMSHttpClient } from '../utils/DotCMSHttpClient';
-import { DotCMSContent, DotCMSError } from '../models';
+import { DotCMSContent, DotCMSContentQuery, DotCMSError } from '../models';
+
+function populateQueryUrl(params: DotCMSContentQuery): string {
+    let url = '';
+
+    const attrs = {
+        depth: `/depth/${params.depth}`,
+        limit: `/limit/${params.limit}`,
+        offset: `/offset/${params.offset}`,
+        orderBy: `/orderby/${params.orderBy}`
+    };
+
+    for (const key of Object.keys(params)) {
+        url += key !== 'query' ? attrs[key] : '';
+    }
+
+    return url;
+}
 
 /**
  * Save/Publish the information of DotCMS Content
@@ -12,6 +29,12 @@ export class DotApiContent {
         this.dotCMSHttpClient = httpClient;
     }
 
+    query(params: DotCMSContentQuery): Promise<Response> {
+        let url = `/api/content/query/${params.query}`;
+        url += populateQueryUrl(params);
+        return this.doRequest(url);
+    }
+
     save<Content extends DotCMSContent>(params: Content): Promise<Response> {
         return this.doRequest('/api/content/save/1', params);
     }
@@ -22,12 +45,12 @@ export class DotApiContent {
 
     private async doRequest<Content extends DotCMSContent>(
         url: string,
-        params: Content
+        params?: Content
     ): Promise<Response> {
         const response = await this.dotCMSHttpClient.request({
             url,
-            method: 'POST',
-            body: JSON.stringify(params)
+            method: params ? 'POST' : 'GET',
+            body: params ? JSON.stringify(params) : ''
         });
 
         if (response.status !== 200) {
--- projects/dotcms/src/lib/models/DotCMSContent.model.ts
+++ projects/dotcms/src/lib/models/DotCMSContent.model.ts
@@ -1,3 +1,11 @@
 export interface DotCMSContent {
     stName: string;
 }
+
+export interface DotCMSContentQuery {
+    depth?: string;
+    limit?: string;
+    offset?: string;
+    orderBy?: string;
+    query: string;
+}
