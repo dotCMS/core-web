237ebd7d30c1fac8f2da9164b2e37925c68ea693
Humberto Morera <humberto.morera@dotcms.com>
2020-07-21 22:29:57 -0600

    clean up

--- src/app/view/components/_common/dot-download-bundle-dialog/dot-download-bundle-dialog.component.ts
+++ src/app/view/components/_common/dot-download-bundle-dialog/dot-download-bundle-dialog.component.ts
@@ -60,7 +60,6 @@ export class DotDownloadBundleDialogComponent implements OnInit, OnDestroy {
                         });
                 }
             });
-        // this.subscribeToWSEvents();
     }
 
     ngOnDestroy(): void {
