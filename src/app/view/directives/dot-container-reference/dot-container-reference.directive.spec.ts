a38d17082af006399f43509af0f6250a04b24cd0
Humberto Morera <humberto.morera@dotcms.com>
2020-07-30 23:37:40 -0600

    progress

---
+++ src/app/view/directives/dot-container-reference/dot-container-reference.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DotContainerReferenceDirective } from './dot-container-reference.directive';
+
+describe('DotContainerReferenceDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DotContainerReferenceDirective();
+    expect(directive).toBeTruthy();
+  });
+});
