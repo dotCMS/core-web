3d231cffde75172b6100fc26c3b78cf365cb0bfb
Humberto Morera <humberto.morera@dotcms.com>
2020-08-14 00:20:14 -0600

    containerref test

--- src/app/view/directives/dot-container-reference/dot-container-reference.directive.spec.ts
+++ src/app/view/directives/dot-container-reference/dot-container-reference.directive.spec.ts
@@ -1,8 +1,56 @@
-//  import { DotContainerReferenceDirective } from './dot-container-reference.directive';
+import { DotContainerReferenceDirective } from './dot-container-reference.directive';
 
-fdescribe('DotContainerReferenceDirective', () => {
-  it('should create an instance', () => {
-    //const directive = new DotContainerReferenceDirective();
-    //expect(directive).toBeTruthy();
-  });
+import {
+    ElementRef,
+    Injector,
+    ViewContainerRef,
+    ComponentRef,
+    EmbeddedViewRef,
+    ViewRef
+} from '@angular/core';
+
+class TestViewContainerRef extends ViewContainerRef {
+    readonly element: ElementRef;
+    readonly injector: Injector;
+    readonly length: number;
+    readonly parentInjector: Injector;
+
+    clear(): void {}
+
+    createComponent<C>(): ComponentRef<C> {
+        return undefined;
+    }
+
+    createEmbeddedView<C>(): EmbeddedViewRef<C> {
+        return undefined;
+    }
+
+    detach(): ViewRef | null {
+        return undefined;
+    }
+
+    get(): ViewRef | null {
+        return undefined;
+    }
+
+    indexOf(): number {
+        return 0;
+    }
+
+    insert(): ViewRef {
+        return undefined;
+    }
+
+    move(): ViewRef {
+        return undefined;
+    }
+
+    remove(): void {}
+}
+
+describe('DotContainerReferenceDirective', () => {
+    it('should create an instance', () => {
+        const directive = new DotContainerReferenceDirective(new TestViewContainerRef());
+        expect(directive).toBeTruthy();
+    });
 });
