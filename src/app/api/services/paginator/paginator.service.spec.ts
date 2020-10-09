c320d6695764f35c76dec72b752a5acf5b77f4c6
Freddy Montes <freddymontes@gmail.com>
2020-10-09 07:05:09 -0600

    Fix tests: PaginatorService

--- src/app/api/services/paginator/paginator.service.spec.ts
+++ src/app/api/services/paginator/paginator.service.spec.ts
@@ -5,7 +5,7 @@ import { CoreWebService } from 'dotcms-js';
 import { CoreWebServiceMock } from '@tests/core-web.service.mock';
 import { HttpHeaders } from '@angular/common/http';
 
-describe('PaginatorService setting', () => {
+describe('PaginatorService', () => {
     let injector: TestBed;
     let paginatorService: PaginatorService;
     let httpMock: HttpTestingController;
@@ -48,7 +48,7 @@ describe('PaginatorService setting', () => {
         paginatorService.setExtraParams('name', 'John');
         paginatorService.deleteExtraParams('name');
 
-        expect(paginatorService.extraParams.get('name')).toBeNull();
+        expect(paginatorService.extraParams.get('name')).toBeUndefined();
     });
 
     afterEach(() => {
