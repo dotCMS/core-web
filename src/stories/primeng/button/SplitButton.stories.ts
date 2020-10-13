eb4c0c585dc5cdc4bbe7b8e780e76123105afb93
Ivor <ivorpad@users.noreply.github.com>
2020-10-14 00:28:56 +0200

    PrimeNG: Update spacing variables
    Co-authored-by: Freddy Montes <freddymontes@gmail.com>

--- src/stories/primeng/button/SplitButton.stories.ts
+++ src/stories/primeng/button/SplitButton.stories.ts
@@ -2,31 +2,33 @@
 import { Meta, Story } from '@storybook/angular/types-6-0';
 import { moduleMetadata } from '@storybook/angular';
 import { SplitButtonModule } from 'primeng/splitbutton';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export default {
-  title: 'PrimeNG/Button/SplitButton',
-  decorators: [
-    moduleMetadata({
-      imports: [SplitButtonModule],
-    }),
-  ],
-  parameters: {
-    layout: 'centered',
-    docs: {
-      description: {
-        component:
-          'SplitButton groups a set of commands in an overlay with a default command: https://primefaces.org/primeng/showcase/#/splitbutton',
-      },
+    title: 'PrimeNG/Button/SplitButton',
+    decorators: [
+        moduleMetadata({
+            imports: [SplitButtonModule, BrowserAnimationsModule]
+        })
+    ],
+    parameters: {
+        layout: 'centered',
+        docs: {
+            description: {
+                component:
+                    'SplitButton groups a set of commands in an overlay with a default command: https://primefaces.org/primeng/showcase/#/splitbutton'
+            }
+        }
     },
-  },
+    args: {}
 } as Meta;
 
 const BasicTemplate = `
   <p>
-    <p-splitButton label="Save" icon="pi pi-check"></p-splitButton>
+    <p-splitButton label="Save" icon="pi pi-check" [model]="items"></p-splitButton>
   </p>  
   <p>
-    <p-splitButton label="Save" icon="pi pi-check" styleClass="p-button-secondary"></p-splitButton>
+    <p-splitButton label="Save" icon="pi pi-check" styleClass="p-button-secondary" [model]="items"></p-splitButton>
   </p>
   <p>
     <p-splitButton label="Save" icon="pi pi-check" styleClass="p-button-disabled"></p-splitButton>
@@ -34,7 +36,24 @@ const BasicTemplate = `
 `;
 export const Basic: Story = () => {
   return {
-    template: BasicTemplate,
+      template: BasicTemplate,
+      props: {
+          items: [
+              {
+                  label: 'Update',
+                  icon: 'pi pi-refresh',
+                  command: () => {}
+              },
+              {
+                  label: 'Delete',
+                  icon: 'pi pi-times',
+                  command: () => {}
+              },
+              { label: 'Angular.io', icon: 'pi pi-info', command: () => {} },
+              { separator: true },
+              { label: 'Setup', icon: 'pi pi-cog', command: () => {} }
+          ]
+      }
   };
 };
 
