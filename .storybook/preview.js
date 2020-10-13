3affdcf55f22d2f81328df188c7985a8780a54cf
Freddy Montes <freddymontes@gmail.com>
2020-10-13 09:35:04 -0600

    add storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,7 @@
+import { setCompodocJson } from '@storybook/addon-docs/angular';
+import docJson from '../documentation.json';
+setCompodocJson(docJson);
+
+export const parameters = {
+    actions: { argTypesRegex: '^on[A-Z].*' }
+};
