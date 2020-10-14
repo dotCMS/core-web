1f8531b4beefe4a15d4ae5672d20396e5aa4173b
Freddy Montes <freddy@dotcms.com>
2020-10-14 12:55:20 -0600

    Unify base size and heights and replace form__group by p-field (#1457)
    * PrimeNG: unify base font-size, add horizontal form and fix button height
    * DotCMS: replace form__group class in favor of primengs one

--- src/stories/dotcms/form/DotCMSForms.stories.ts
+++ src/stories/dotcms/form/DotCMSForms.stories.ts
@@ -12,199 +12,301 @@ import { AutoCompleteModule } from 'primeng/autocomplete';
 import { moduleMetadata } from '@storybook/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CalendarModule } from 'primeng/calendar';
+import { MultiSelectModule } from 'primeng/multiselect';
 
 export default {
-  title: 'DotCMS/Form/Forms',
-  parameters: {
-    docs: {
-      description: {
-        component:
-          'Text, icon, buttons and other content can be grouped next to an input.: https://primefaces.org/primeng/showcase/#/inputgroup',
-      },
+    title: 'DotCMS/Forms    ',
+
+    parameters: {
+        docs: {
+            description: {
+                component:
+                    'Text, icon, buttons and other content can be grouped next to an input.: https://primefaces.org/primeng/showcase/#/inputgroup'
+            }
+        }
     },
-  },
-  decorators: [
-    moduleMetadata({
-      imports: [
-        FormsModule,
-        InputTextModule,
-        ButtonModule,
-        CheckboxModule,
-        RadioButtonModule,
-        RippleModule,
-        BrowserAnimationsModule,
-        InputTextareaModule,
-        DropdownModule,
-        ChipsModule,
-        AutoCompleteModule,
-        CalendarModule,
-      ],
-    }),
-  ],
-  args: {
-    options: [
-      { label: 'Select Host', value: null },
-      { label: 'demo.dotcms.com', value: { id: 1, name: 'demo.dotcms.com' } },
-      { label: 'System Host', value: { id: 2, name: 'System Host' } },
+    decorators: [
+        moduleMetadata({
+            imports: [
+                FormsModule,
+                InputTextModule,
+                ButtonModule,
+                CheckboxModule,
+                RadioButtonModule,
+                RippleModule,
+                BrowserAnimationsModule,
+                InputTextareaModule,
+                DropdownModule,
+                ChipsModule,
+                AutoCompleteModule,
+                CalendarModule,
+                MultiSelectModule
+            ]
+        })
     ],
-    actions: [
-      { label: 'System Workflow', value: null },
-      { label: 'Save', value: { id: 1, name: 'Save' } },
-      { label: 'Publish', value: { id: 2, name: 'Publish' } },
-      { label: 'Delete', value: { id: 3, name: 'Delete' } },
-    ],
-    values: ['hiking'],
-    results: [
-      { name: 'Blogs', id: 1 },
-      { name: 'System Workflow', id: 2 },
-    ],
-    // tslint:disable-next-line: typedef
-    search() {
-      this.results = [
-        { name: 'Blogs', id: 1 },
-        { name: 'System Workflow', id: 2 },
-      ];
-    },
-  },
+    args: {
+        options: [
+            { label: 'Select Host', value: null },
+            { label: 'demo.dotcms.com', value: { id: 1, name: 'demo.dotcms.com' } },
+            { label: 'System Host', value: { id: 2, name: 'System Host' } }
+        ],
+        actions: [
+            { label: 'System Workflow', value: null },
+            { label: 'Save', value: { id: 1, name: 'Save' } },
+            { label: 'Publish', value: { id: 2, name: 'Publish' } },
+            { label: 'Delete', value: { id: 3, name: 'Delete' } }
+        ],
+        values: ['hiking'],
+        results: [
+            { name: 'Blogs', id: 1 },
+            { name: 'System Workflow', id: 2 }
+        ],
+        cities: [
+            { name: 'New York', code: 'NY' },
+            { name: 'Rome', code: 'RM' },
+            { name: 'London', code: 'LDN' },
+            { name: 'Istanbul', code: 'IST' },
+            { name: 'Paris', code: 'PRS' }
+        ],
+        selectedCities: [{ name: 'Paris', code: 'PRS' }],
+        search() {
+            this.results = [
+                { name: 'Blogs', id: 1 },
+                { name: 'System Workflow', id: 2 }
+            ];
+        }
+    }
 } as Meta;
 
 const TextInputDefault = `
-    <div class="form__group">
-      <label for="widgetName">Widget Name</label>
-      <input id="name" aria-describedby="widgetName-help" pInputText />
-      <small id="widgetName-help">Enter the widget name.</small>
-    </div>
-    <div class="form__group">
-      <label for="slug">Slug</label>
-      <input id="slug" aria-describedby="slug-help" class="p-invalid" pInputText />
-      <small id="slug-help" class="p-invalid">Slug must be unique.</small>
-    </div>
+<div class="p-field">
+    <label for="widgetName">Widget Name</label>
+    <input id="name" aria-describedby="widgetName-help" pInputText />
+    <small id="widgetName-help">Enter the widget name.</small>
+</div>
+<div class="p-field">
+    <label for="slug">Slug</label>
+    <input id="slug" aria-describedby="slug-help" class="p-invalid" pInputText />
+    <small id="slug-help" class="p-invalid">Slug must be unique.</small>
+</div>
 `;
 
 const TextInputFloatingLabel = `
-    <div class="form__group">
-      <span class="p-float-label">
-          <input id="widgetName" type="text" pInputText>
-          <label for="widgetName">Widget Name</label>
-      </span>
-    </div>
-    <div class="form__group">
-      <span class="p-float-label">
-          <input id="description" type="text" pInputText>
-          <label for="description">Description</label>
-      </span>
-    </div>
+<div class="p-field">
+    <span class="p-float-label">
+        <input id="pInputText" type="text" pInputText>
+        <label for="pInputText">Input Text</label>
+    </span>
+</div>
 `;
 
 const InputTemplate = (input) => {
-  return `<div class="p-fluid" style="width: 400px; margin: 0 auto;">
-     ${input}
-      <div class="form__group">
-        <label for="dropdown">Site or Folder</label>
-        <p-dropdown id="dropdown" [options]="options"></p-dropdown>
-      </div>
-      <div class="form__group">
-      <label for="workflow">Workflow</label>
+    return `
+    <div class="p-fluid" style="width: 400px; margin: 0 auto;">
+    ${input}
+    <div class="p-field">
+        <label for="autoComplete">AutoComplete</label>
         <p-autoComplete
-          id="workflow"
-          (completeMethod)="search($event)"
-          [(ngModel)]="text"
-          [dropdown]="true"
-          [suggestions]="results"
-          field="name"
+            id="autoComplete"
+            (completeMethod)="search($event)"
+            [(ngModel)]="text"
+            [dropdown]="true"
+            [suggestions]="results"
+            field="name"
         ></p-autoComplete>
-      </div>
+    </div>
 
-       <div class="form__group">
-        <label for="options">Default Action</label>
-        <p-dropdown id="options" [options]="actions"></p-dropdown>
-      </div>
+    <div class="p-field">
+        <label for="dropdown">Dropdown</label>
+        <p-dropdown
+            id="dropdown"
+            inputId="dropdown"
+            selectId="dropdown"
+            [options]="actions"
+        ></p-dropdown>
+    </div>
 
-      <div class="form__group">
-         <label for="publishDate">Publish Date Field</label>
-         <p-calendar [showTime]="true" inputId="publishDate"></p-calendar>
-        </div>
-      <div class="form__group">
-        <label for="expireDate">Expire Date Field</label>
-        <p-calendar [showTime]="true" inputId="expireDate"></p-calendar>
-      </div>
+    <div class="p-field">
+        <label for="calendar">Calendar</label>
+        <p-calendar [showTime]="true" inputId="calendar"></p-calendar>
+    </div>
 
-      <div class="form__group">
+    <div class="p-field">
         <label for="tags">Tags</label>
-        <p-chips id="tags" [(ngModel)]="values" ></p-chips>
-      </div>
-
-    <div class="form__group">
-      <label>Cities</label>
-
-      <div class="p-field-checkbox">
-        <p-checkbox name="group1" value="New York" inputId="ny"></p-checkbox>
-        <label for="ny">New York</label>
-      </div>
-      <div class="p-field-checkbox">
-        <p-checkbox name="group1" value="San Francisco" inputId="sf"></p-checkbox>
-        <label for="sf">San Francisco</label>
-      </div>
-      <div class="p-field-checkbox">
-        <p-checkbox name="group1" value="Los Angeles" inputId="la"></p-checkbox>
-        <label for="la">Los Angeles</label>
-      </div>
-      <div class="p-field-checkbox">
-        <p-checkbox name="group1" value="Chicago" inputId="ch"></p-checkbox>
-        <label for="ch">Chicago</label>
-      </div>
+        <p-chips id="tags" [(ngModel)]="values"></p-chips>
     </div>
 
-  <div class="form__group">
-    <label>Sizes</label>
-    <div class="p-field-radiobutton">
-      <p-radioButton name="size" value="Small" inputId="size1"></p-radioButton>
-      <label for="size1">Small</label>
+    <div class="p-field">
+        <label for="multiSelect">Multiselect</label>
+        <p-multiSelect
+            [options]="cities"
+            id="multiSelect"
+            defaultLabel="Select a City"
+            optionLabel="name"
+        ></p-multiSelect>
     </div>
-    <div class="p-field-radiobutton">
-      <p-radioButton name="size" value="M"edium inputId="size2"></p-radioButton>
-      <label for="size2">Medium</label>
-    </div>
-    <div class="p-field-radiobutton">
-      <p-radioButton name="size" value="Big" inputId="size3"></p-radioButton>
-      <label for="size3">Big</label>
-    </div>
-  </div>
 
-  <div class="form__group">
-    <label for="comments">Comments</label>
-    <textarea id="comments" pInputTextarea placeholder="Some placeholder" [rows]="10"></textarea>
-  </div>
-  </div>`;
+    <div class="p-field">
+        <label>Checkbox</label>
+
+        <div class="p-field-checkbox">
+            <p-checkbox name="group1" value="New York" inputId="ny"></p-checkbox>
+            <label for="ny">New York</label>
+        </div>
+        <div class="p-field-checkbox">
+            <p-checkbox name="group1" value="San Francisco" inputId="sf"></p-checkbox>
+            <label for="sf">San Francisco</label>
+        </div>
+        <div class="p-field-checkbox">
+            <p-checkbox name="group1" value="Los Angeles" inputId="la"></p-checkbox>
+            <label for="la">Los Angeles</label>
+        </div>
+        <div class="p-field-checkbox">
+            <p-checkbox name="group1" value="Chicago" inputId="ch"></p-checkbox>
+            <label for="ch">Chicago</label>
+        </div>
+    </div>
+
+    <div class="p-field">
+        <label>Radio</label>
+        <div class="p-field-radiobutton">
+            <p-radioButton
+                name="size"
+                [(ngModel)]="city"
+                value="Small"
+                inputId="size1"
+            ></p-radioButton>
+            <label for="size1">Small</label>
+        </div>
+        <div class="p-field-radiobutton">
+            <p-radioButton
+                name="size"
+                [(ngModel)]="city"
+                value="Medium"
+                inputId="size2"
+            ></p-radioButton>
+            <label for="size2">Medium</label>
+        </div>
+        <div class="p-field-radiobutton">
+            <p-radioButton
+                name="size"
+                [(ngModel)]="city"
+                value="Big"
+                inputId="size3"
+            ></p-radioButton>
+            <label for="size3">Big</label>
+        </div>
+    </div>
+
+    <div class="p-field">
+        <label for="comments">Comments</label>
+        <textarea
+            id="comments"
+            pInputTextarea
+            placeholder="Some placeholder"
+            [rows]="10"
+        ></textarea>
+    </div>
+</div>
+    `;
 };
 
-export const Default: Story = (props) => {
-  return {
-    template: InputTemplate(TextInputDefault),
-    props
-  };
+export const Vertical: Story = (props) => {
+    return {
+        template: InputTemplate(TextInputDefault),
+        props
+    };
 };
 
-Default.parameters = {
-  docs: {
-    source: {
-      code: InputTemplate(TextInputDefault),
-    },
-  },
+Vertical.parameters = {
+    docs: {
+        source: {
+            code: InputTemplate(TextInputDefault)
+        }
+    }
+};
+
+const HorizontalTemplate = `
+<h3>Inline</h3>
+<div class="p-formgroup-inline">
+    <div class="p-field">
+        <label for="firstname" class="p-sr-only">Firstname</label>
+        <input id="firstname" type="text" pInputText placeholder="Firstname" />
+    </div>
+    <div class="p-field">
+        <label for="dropdown" class="p-sr-only">Dropdown</label>
+        <p-dropdown
+            id="dropdown"
+            inputId="dropdown"
+            selectId="dropdown"
+            [options]="options"
+        ></p-dropdown>
+    </div>
+    <div class="p-field">
+        <label for="lastname" class="p-sr-only">AutoComplete</label>
+        <p-autoComplete
+            id="workflow"
+            (completeMethod)="search($event)"
+            [(ngModel)]="text"
+            [dropdown]="true"
+            [suggestions]="results"
+            field="name"
+        ></p-autoComplete>
+    </div>
+    <div class="p-field">
+        <label for="lastname" class="p-sr-only">AutoComplete</label>
+        <p-multiSelect
+            [options]="cities"
+            defaultLabel="Select a City"
+            optionLabel="name"
+        ></p-multiSelect>
+    </div>
+    <button pButton type="button" label="Submit"></button>
+</div>
+
+
+<h3>Grid</h3>
+<div class="p-fluid p-formgrid p-grid">
+    <div class="p-field p-col">
+        <label for="firstname1">Firstname</label>
+        <input id="firstname1" type="text" pInputText>
+    </div>
+    <div class="p-field p-col">
+        <label for="lastname1">Lastname</label>
+        <input id="lastname1" type="text" pInputText>
+    </div>
+</div>
+
+`;
+
+export const Horizontal: Story = (props) => {
+    return {
+        template: HorizontalTemplate,
+        props
+    };
+};
+
+Horizontal.parameters = {
+    layout: 'centered',
+    docs: {
+        source: {
+            code: HorizontalTemplate
+        }
+    }
 };
 
 export const FloatingLabel: Story = (props) => {
-  return {
-    template: InputTemplate(TextInputFloatingLabel),
-    props
-  };
+    return {
+        template: InputTemplate(TextInputFloatingLabel),
+        props
+    };
 };
 
 FloatingLabel.parameters = {
-  template: {},
-  docs: {
-    source: {
-      code: InputTemplate(TextInputFloatingLabel),
-    },
-  },
+    template: {},
+    docs: {
+        source: {
+            code: InputTemplate(TextInputFloatingLabel)
+        }
+    }
 };
