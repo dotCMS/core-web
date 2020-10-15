b619b413700884a8660c123121f22d1782cbcb3b
Freddy Montes <freddymontes@gmail.com>
2020-10-15 09:09:00 -0600

    DotCMS Add searchable-dropdown.component to sb

--- src/app/view/components/_common/searchable-dropdown/component/searchable-dropdown.component.ts
+++ src/app/view/components/_common/searchable-dropdown/component/searchable-dropdown.component.ts
@@ -138,6 +138,7 @@ export class SearchableDropdownComponent
     }
 
     ngAfterContentInit() {
+        this.totalRecords = this.totalRecords || this.data.length;
         this.templates.forEach((item: PrimeTemplate) => {
             if (item.getType() === 'listItem') {
                 this.externalItemListTemplate = item.template;
