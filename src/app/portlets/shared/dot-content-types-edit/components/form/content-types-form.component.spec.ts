66aabb3fc4de7e08a2b247935ffa8ebc21749021
Humberto Morera <humberto.morera@dotcms.com>
2020-10-19 15:14:59 -0600

    fix

--- src/app/portlets/shared/dot-content-types-edit/components/form/content-types-form.component.spec.ts
+++ src/app/portlets/shared/dot-content-types-edit/components/form/content-types-form.component.spec.ts
@@ -34,7 +34,6 @@ import { DotPageSelectorModule } from '@components/_common/dot-page-selector/dot
 import { DotDirectivesModule } from '@shared/dot-directives.module';
 import { DotIconModule } from '@components/_common/dot-icon/dot-icon.module';
 import { DotIconButtonModule } from '@components/_common/dot-icon-button/dot-icon-button.module';
-import { MdInputTextModule } from '@directives/md-inputtext/md-input-text.module';
 import { DotFieldHelperModule } from '@components/dot-field-helper/dot-field-helper.module';
 import {
     dotcmsContentTypeFieldBasicMock,
@@ -169,7 +168,6 @@ describe('ContentTypesFormComponent', () => {
                 DotWorkflowsSelectorFieldModule,
                 DropdownModule,
                 InputTextModule,
-                MdInputTextModule,
                 OverlayPanelModule,
                 ReactiveFormsModule,
                 RouterTestingModule,
--- src/app/view/components/_common/dot-page-selector/dot-page-selector.component.spec.ts
+++ src/app/view/components/_common/dot-page-selector/dot-page-selector.component.spec.ts
@@ -9,7 +9,6 @@ import { DotPageSelectorService } from './service/dot-page-selector.service';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { AutoComplete } from 'primeng/autocomplete';
 import { DotDirectivesModule } from '@shared/dot-directives.module';
-import { MdInputTextModule } from '@directives/md-inputtext/md-input-text.module';
 import {
     DotPageSelectorResults,
     DotPageSeletorItem
@@ -121,7 +120,7 @@ const messageServiceMock = new MockDotMessageService({
 const config = host => {
     return {
         declarations: [host, DotPageSelectorComponent],
-        imports: [DotDirectivesModule, MdInputTextModule, DotFieldHelperModule],
+        imports: [DotDirectivesModule, DotFieldHelperModule],
         providers: [
             { provide: DotPageSelectorService, useClass: MockDotPageSelectorService },
             { provide: LoginService, useClass: LoginServiceMock },
