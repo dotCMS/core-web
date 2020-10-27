fac7afc3ac6078bcc021b6b0884058d2e98ecbe6
Freddy Montes <freddymontes@gmail.com>
2020-10-27 12:28:18 -0600

    Render basic template designer

--- src/app/portlets/dot-templates/dot-template-designer/dot-template-designer.component.ts
+++ src/app/portlets/dot-templates/dot-template-designer/dot-template-designer.component.ts
@@ -1,5 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import {
+    DotLayoutBody,
+    DotLayoutColumn,
+    DotLayoutRow
+} from '@portlets/dot-edit-page/shared/models';
+import { TemplateContainersCacheService } from '@portlets/dot-edit-page/template-containers-cache.service';
+import { CONTAINER_SOURCE } from '@shared/models/container/dot-container.model';
+
+const containers = {
+    '//demo.dotcms.com/application/containers/default/': {
+        container: {
+            categoryId: 'f04b5078-1679-4965-bb12-446d3cb5948f',
+            deleted: false,
+            friendlyName: 'container',
+            identifier: '69b3d24d-7e80-4be6-b04a-d352d16493ee',
+            name: 'Default',
+            type: 'containers',
+            source: CONTAINER_SOURCE.FILE,
+            parentPermissionable: {
+                hostname: 'SYSTEM_HOST'
+            }
+        }
+    }
+};
 
 @Component({
     selector: 'dot-dot-template-designer',
@@ -7,11 +32,48 @@ import { ActivatedRoute } from '@angular/router';
     styleUrls: ['./dot-template-designer.component.scss']
 })
 export class DotTemplateDesignerComponent implements OnInit {
-    constructor(private activatedRoute: ActivatedRoute) {}
+    form: FormGroup;
+
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private fb: FormBuilder,
+        private templateContainersCacheService: TemplateContainersCacheService
+    ) {}
 
     ngOnInit(): void {
+        this.templateContainersCacheService.set(containers);
         this.activatedRoute.data.subscribe((res) => {
-            console.log(res);
+            console.log(res.template.layout);
+
+            this.form = this.fb.group({
+                layout: this.fb.group({
+                    body: this.cleanUpBody(res.template.layout.body) || {},
+                    header: res.template.layout.header,
+                    footer: res.template.layout.footer,
+                    sidebar: {}
+                })
+            });
         });
+    }
+
+    // this is dupe, need to extract
+    private cleanUpBody(body: DotLayoutBody): DotLayoutBody {
+        return body
+            ? {
+                  rows: body.rows.map((row: DotLayoutRow) => {
+                      return {
+                          ...row,
+                          columns: row.columns.map((column: DotLayoutColumn) => {
+                              return {
+                                  containers: column.containers,
+                                  leftOffset: column.leftOffset,
+                                  width: column.width,
+                                  styleClass: column.styleClass
+                              };
+                          })
+                      };
+                  })
+              }
+            : null;
     }
 }
--- src/app/portlets/dot-templates/dot-template-designer/dot-template-designer.module.ts
+++ src/app/portlets/dot-templates/dot-template-designer/dot-template-designer.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { DotTemplateDesignerRoutingModule } from './dot-template-designer-routing.module';
+import { DotLayoutDesignerModule } from '@portlets/dot-edit-page/layout/dot-edit-layout-designer/components/dot-layout-designer/dot-layout-designer.module';
+import { DotTemplateDesignerComponent } from './dot-template-designer.component';
 
 @NgModule({
-    declarations: [],
-    imports: [CommonModule, DotTemplateDesignerRoutingModule]
+    declarations: [DotTemplateDesignerComponent],
+    imports: [
+        CommonModule,
+        DotTemplateDesignerRoutingModule,
+        DotLayoutDesignerModule,
+        FormsModule,
+        ReactiveFormsModule
+    ]
 })
 export class DotTemplateDesignerModule {}
