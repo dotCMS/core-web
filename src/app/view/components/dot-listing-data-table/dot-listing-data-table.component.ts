e9b5bbb78c875e02e93d58eb6597e6b5746ed0f4
Freddy Montes <freddymontes@gmail.com>
2020-10-30 13:43:36 -0600

    Extra fix after merge

--- src/app/view/components/dot-listing-data-table/dot-listing-data-table.component.ts
+++ src/app/view/components/dot-listing-data-table/dot-listing-data-table.component.ts
@@ -20,7 +20,6 @@ import { DataTableColumn } from '@models/data-table/data-table-column';
 import { LoggerService } from 'dotcms-js';
 import { FormatDateService } from '@services/format-date-service';
 import { PaginatorService, OrderDirection } from '@services/paginator';
-import { DotDataTableAction } from '@models/data-table/dot-data-table-action';
 import { take } from 'rxjs/operators';
 
 function tableFactory(dotListingDataTableComponent: DotListingDataTableComponent) {
@@ -49,7 +48,7 @@ export class DotListingDataTableComponent implements OnChanges, OnInit {
     @Input() sortField: string;
     @Input() multipleSelection = false;
     @Input() paginationPerPage = 40;
-    @Input() actions: DotDataTableAction[];
+    @Input() actions: ButtonAction[];
     @Input() selectionMode = 'single';
     @Input() dataKey = '';
     @Input() checkbox = false;
