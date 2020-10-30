b0aad54c5b61a5306327c38363d9e803b585b122
Freddy Montes <freddymontes@gmail.com>
2020-10-30 13:55:41 -0600

    Remove unnecessary component

--- src/app/portlets/dot-templates/dot-templates.module.ts
+++ src/app/portlets/dot-templates/dot-templates.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DotTemplatesRoutingModule } from './dot-templates-routing.module';
-import { DotTemplatesComponent } from './dot-templates.component';
 import { DotTemplatesService } from '@services/dot-templates/dot-templates.service';
 import { DotTemplateListModule } from '@portlets/dot-templates/dot-template-list/dot-template-list.module';
 
 @NgModule({
-    declarations: [DotTemplatesComponent],
+    declarations: [],
     imports: [
         CommonModule,
         DotTemplatesRoutingModule,
