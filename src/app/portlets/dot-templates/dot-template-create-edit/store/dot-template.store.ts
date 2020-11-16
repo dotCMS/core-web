6f64675101f93b630e313ab00288a9afc24f6d18
Freddy Montes <freddymontes@gmail.com>
2020-11-16 10:16:49 -0600

    Update theme id hardcoded

--- src/app/portlets/dot-templates/dot-template-create-edit/store/dot-template.store.ts
+++ src/app/portlets/dot-templates/dot-template-create-edit/store/dot-template.store.ts
@@ -59,7 +59,7 @@ const EMPTY_TEMPLATE_DESIGN: DotTemplateItemDesign = {
         title: '',
         width: null
     },
-    theme: '',
+    theme: 'd7b0ebc2-37ca-4a5a-b769-e8a3ff187661', // TODO: use theme selector
     containers: {}
 };
 
