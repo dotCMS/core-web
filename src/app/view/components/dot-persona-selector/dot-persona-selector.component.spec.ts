3deb20c6f9ac6c57b9203fff4b7cc5b64c3c9588
ivorpad <ivorjpc@gmail.com>
2020-11-17 15:59:59 +0100

    Added test for overlayWidth

--- src/app/view/components/dot-persona-selector/dot-persona-selector.component.spec.ts
+++ src/app/view/components/dot-persona-selector/dot-persona-selector.component.spec.ts
@@ -129,9 +129,10 @@ describe('DotPersonaSelectorComponent', () => {
         expect(paginatorService.getWithOffset).toHaveBeenCalledWith(10);
     });
 
-    it('should set dot-searchable-dropdown with right attributes', () => {
+    fit('should set dot-searchable-dropdown with right attributes', () => {
         expect(dropdown.componentInstance.labelPropertyName).toBe('name');
         expect(dropdown.componentInstance.width).toBe('448px');
+        expect(dropdown.componentInstance.overlayWidth).toBe('300px');
         expect(dropdown.componentInstance.rows).toBe(10);
         expect(dropdown.componentInstance.totalRecords).toBe(1);
     });
