b52c51af16a88e889f13f776e0fc324f38691453
Freddy Montes <freddymontes@gmail.com>
2020-11-24 15:51:08 -0600

    Create tests for DotPortletBoxComponent

--- src/app/view/components/dot-portlet-base/components/dot-portlet-box/dot-portlet-box.component.spec.ts
+++ src/app/view/components/dot-portlet-base/components/dot-portlet-box/dot-portlet-box.component.spec.ts
@@ -1,24 +1,33 @@
+import { Component, DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { DotPortletBoxComponent } from './dot-portlet-box.component';
 
+@Component({
+    template: `<dot-portlet-box><div>Hello World</div></dot-portlet-box>`
+})
+class DefaultTestHostComponent {}
+
 describe('DotPortletBoxComponent', () => {
-    let component: DotPortletBoxComponent;
+    let component: DebugElement;
     let fixture: ComponentFixture<DotPortletBoxComponent>;
+    let de: DebugElement;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [DotPortletBoxComponent]
+            declarations: [DotPortletBoxComponent, DefaultTestHostComponent]
         }).compileComponents();
     });
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(DotPortletBoxComponent);
-        component = fixture.componentInstance;
+        fixture = TestBed.createComponent(DefaultTestHostComponent);
+        de = fixture.debugElement;
+        component = de.query(By.css('dot-portlet-box'));
         fixture.detectChanges();
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    it('should show children', () => {
+        expect(component.nativeNode.innerHTML).toBe('<div>Hello World</div>');
     });
 });
