23e0654d3137c37924accb2f6769f9e442a43885
Freddy Montes <freddymontes@gmail.com>
2020-11-24 14:12:20 -0600

    Make it run

--- src/app/view/pipes/dot-message/dot-message-pipe.module.ts
+++ src/app/view/pipes/dot-message/dot-message-pipe.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DotMessagePipe } from './dot-message.pipe';
+import { DotMessageService } from '@services/dot-message/dot-messages.service';
 
 @NgModule({
     imports: [CommonModule],
     declarations: [DotMessagePipe],
-    exports: [DotMessagePipe]
+    exports: [DotMessagePipe],
+    providers: [DotMessageService]
 })
 export class DotMessagePipeModule {}
