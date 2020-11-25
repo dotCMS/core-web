6c5a20a6313c8c452c669f8f374ef44c8bd03d94
Freddy Montes <freddymontes@gmail.com>
2020-11-25 08:44:02 -0600

    Create tests for DotTemplateBuilderComponent

--- src/app/portlets/dot-templates/dot-template-create-edit/dot-template-builder/dot-template-builder.module.ts
+++ src/app/portlets/dot-templates/dot-template-create-edit/dot-template-builder/dot-template-builder.module.ts
@@ -4,9 +4,16 @@ import { DotTemplateBuilderComponent } from './dot-template-builder.component';
 import { DotEditLayoutDesignerModule } from '@components/dot-edit-layout-designer/dot-edit-layout-designer.module';
 import { TabViewModule } from 'primeng/tabview';
 import { DotTemplateAdvancedModule } from '../dot-template-advanced/dot-template-advanced.module';
+import { DotMessagePipeModule } from '@pipes/dot-message/dot-message-pipe.module';
 
 @NgModule({
-    imports: [CommonModule, DotEditLayoutDesignerModule, TabViewModule, DotTemplateAdvancedModule],
+    imports: [
+        CommonModule,
+        DotEditLayoutDesignerModule,
+        DotMessagePipeModule,
+        DotTemplateAdvancedModule,
+        TabViewModule
+    ],
     declarations: [DotTemplateBuilderComponent],
     exports: [DotTemplateBuilderComponent]
 })
