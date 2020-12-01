c0d60a50ee215d2b01f702ae76632f731ae62463
Humberto Morera <humberto.morera@dotcms.com>
2020-12-01 15:03:57 -0600

    feedback

--- src/app/portlets/dot-templates/dot-template-list/dot-template-list.component.spec.ts
+++ src/app/portlets/dot-templates/dot-template-list/dot-template-list.component.spec.ts
@@ -197,7 +197,7 @@ const mockMessageConfig = {
     type: DotMessageType.SIMPLE_MESSAGE
 };
 
-fdescribe('DotTemplateListComponent', () => {
+describe('DotTemplateListComponent', () => {
     let fixture: ComponentFixture<DotTemplateListComponent>;
     let dotListingDataTable: DotListingDataTableComponent;
     let dotTemplatesService: DotTemplatesService;
