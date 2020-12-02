b495b79d00df2f027295dc8526556f5615b3fe5b
Freddy Montes <freddymontes@gmail.com>
2020-12-02 14:58:11 -0600

    Fix messages keys

--- src/app/view/pipes/dot-message/dot-message-pipe.module.ts
+++ src/app/view/pipes/dot-message/dot-message-pipe.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DotMessagePipe } from './dot-message.pipe';
-import { DotMessageService } from '@services/dot-message/dot-messages.service';
 
 @NgModule({
     imports: [CommonModule],
     declarations: [DotMessagePipe],
-    exports: [DotMessagePipe],
-    providers: [DotMessageService]
+    exports: [DotMessagePipe]
 })
 export class DotMessagePipeModule {}
