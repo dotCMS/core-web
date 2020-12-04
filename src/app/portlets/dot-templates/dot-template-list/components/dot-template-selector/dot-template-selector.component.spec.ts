e77b24b955739ae576bdc9e773c051062453ea97
Freddy Montes <freddymontes@gmail.com>
2020-12-04 15:53:48 -0600

    Make DotTemplateSelectorComponent tests run

--- src/app/portlets/dot-templates/dot-template-list/components/dot-template-selector/dot-template-selector.component.spec.ts
+++ src/app/portlets/dot-templates/dot-template-list/components/dot-template-selector/dot-template-selector.component.spec.ts
@@ -1,14 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DotTemplateSelectorComponent } from './dot-template-selector.component';
+import { DynamicDialogRef } from 'primeng/dynamicdialog';
 
-describe('DotTemplateSelectorComponent', () => {
+import { DotMessageService } from '@services/dot-message/dot-messages.service';
+import { MockDotMessageService } from '@tests/dot-message-service.mock';
+import { DotTemplateSelectorComponent } from './dot-template-selector.component';
+import { DotMessagePipeModule } from '@pipes/dot-message/dot-message-pipe.module';
+import { DebugElement } from '@angular/core';
+
+const messageServiceMock = new MockDotMessageService({
+    'templates.template.selector.label.designer': 'Designer',
+    'templates.template.selector.label.advanced': 'Advanced',
+    'templates.template.selector.design':
+        '<b>Template Designer</b> allows you to create templates seamlessly with a set of tools lorem ipsum.',
+    'templates.template.selector.advanced':
+        '<b>Template Advanced</b> allows you to create templates using HTML code'
+});
+
+fdescribe('DotTemplateSelectorComponent', () => {
     let component: DotTemplateSelectorComponent;
     let fixture: ComponentFixture<DotTemplateSelectorComponent>;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [DotTemplateSelectorComponent]
+            declarations: [DotTemplateSelectorComponent],
+            providers: [
+                {
+                    provide: DotMessageService,
+                    useValue: messageServiceMock
+                },
+                {
+                    provide: DynamicDialogRef,
+                    useValue: {
+                        close: () => {}
+                    }
+                }
+            ],
+            imports: [DotMessagePipeModule]
         }).compileComponents();
     });
 
@@ -18,7 +46,9 @@ describe('DotTemplateSelectorComponent', () => {
         fixture.detectChanges();
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    describe('html', () => {
+        it('should create', () => {
+            expect(component).toBeTruthy();
+        });
     });
 });
