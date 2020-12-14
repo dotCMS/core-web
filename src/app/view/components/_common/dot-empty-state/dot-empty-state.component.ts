96b9e451fc4dbf2ffdfb06fe4844c2dd0626f68c
ivorpad <ivorjpc@gmail.com>
2020-12-14 15:01:16 +0100

    Made the empty state component dynamic

--- src/app/view/components/_common/dot-empty-state/dot-empty-state.component.ts
+++ src/app/view/components/_common/dot-empty-state/dot-empty-state.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'dot-empty-state',
@@ -6,7 +6,26 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./dot-empty-state.component.scss']
 })
 export class DotEmptyStateComponent implements OnInit {
+    @Input()
+    rows: number;
+
+    @Input()
+    cols: number[];
+    inputWidth: string;
+
     constructor() {}
 
-    ngOnInit(): void {}
+    /**
+     * Return rows for empty template
+     * @param number n
+     * @returns number[]
+     * @memberof DotEmptyStateComponent
+     */
+    numberOfRows(): number[] {
+        return Array(this.rows).fill(0);
+    }
+
+    ngOnInit(): void {
+        this.inputWidth = `${96.5 / this.cols.length}%`; // 100% - width of checkbox = 96.5%, which is then divided by number of cols
+    }
 }
