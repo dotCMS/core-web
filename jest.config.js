12dcf74ef63eb56dfc743b125238e53f8595e58b
ivorpad <ivorjpc@gmail.com>
2021-03-01 18:17:25 +0100

    Added NX to core-web to handle everything in a monorepo

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/dotcms-ui',
+    '<rootDir>/libs/dot-rules',
+    '<rootDir>/libs/dotcms-js',
+    '<rootDir>/libs/dot-layout-grid',
+    '<rootDir>/libs/dotcms-models',
+    '<rootDir>/libs/dotcms',
+  ],
+};
