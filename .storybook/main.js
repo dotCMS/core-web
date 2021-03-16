33c988946dd5f2638ffe7b37093c425fe65485c6
Freddy Montes <freddymontes@gmail.com>
2021-03-16 13:36:27 -0600

    Add ref as a function

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,24 @@
 module.exports = {
     stories: [],
     addons: ['@storybook/addon-knobs/register', '@storybook/addon-links', '@storybook/addon-essentials'],
-    refs: {
-        dotcmsWebComponents: {
-            title: 'DotCMS Web Components',
-            url: 'https://dotcms.github.io/core-web/dotcms-webcomponents'
+    refs: (config) => {
+        //👇 Retrieves the current environment from the config object
+        const { configType } = config;
+
+        if (configType === 'DEVELOPMENT') {
+            return {
+                dotcmsWebComponents: {
+                    title: 'DotCMS Web Components',
+                    url: 'http://localhost:6600'
+                }
+            };
         }
+
+        return {
+            dotcmsWebComponents: {
+                title: 'DotCMS Web Components',
+                url: 'https://dotcms.github.io/core-web/dotcms-webcomponents'
+            }
+        };
     }
 };
