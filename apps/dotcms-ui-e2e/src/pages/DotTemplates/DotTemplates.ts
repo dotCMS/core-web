4626cfac12dcdf322e08fa9088a4f78f91ac78bf
Alfredo Li <alfredo@dotcms.com>
2021-06-08 09:22:16 -0600

    test run-cypress

--- apps/dotcms-ui-e2e/src/pages/DotTemplates/DotTemplates.ts
+++ apps/dotcms-ui-e2e/src/pages/DotTemplates/DotTemplates.ts
@@ -8,13 +8,13 @@ const CREATE_TEMPLATE_BUTTON = 'dot-action-button dot-icon i';
 class DotTemplates {
     static openPage() {
         cy.visit('/dotAdmin');
-        cy.wait(10000)
-        // cy.intercept('GET', 'v1/templates').as('templates');
+        // cy.wait(10000)
+        cy.intercept('GET', 'v1/templates').as('templates');
         Navigation.visit(URL);
-        cy.wait(10000)
+        // cy.wait(10000)
         Navigation.assertPageUrlIs(URL);
-        cy.wait(5000)
-        // cy.wait('@templates');
+        // cy.wait(5000)
+        cy.wait('@templates');
     }
 
     static checkTemplatesPageLoaded() {
