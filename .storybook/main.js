a07f388be309ae8147b4162ac570145cbd8b5f95
Freddy Montes <freddymontes@gmail.com>
2021-06-17 13:11:55 -0600

    Add sb composition

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,14 @@
 module.exports = {
     stories: [],
-    addons: ['@storybook/addon-knobs/register']
+    addons: ['@storybook/addon-knobs/register'],
+    refs: {
+        webcomponents: {
+            title: 'DotCMS WebComponents',
+            url: 'https://dotcms.github.io/core-web/dotcms-webcomponents'
+        },
+        blockeditor: {
+            title: 'DotCMS Block Editor',
+            url: 'https://dotcms.github.io/core-web/block-editor'
+        }
+    }
 };
