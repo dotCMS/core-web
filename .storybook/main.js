101725aeb78bd4fa3ad3f49a563ceba84bff9829
Freddy Montes <freddymontes@gmail.com>
2021-06-17 13:56:16 -0600

    Another sb composition try

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,4 @@
 module.exports = {
     stories: [],
-    addons: ['@storybook/addon-knobs/register'],
-    refs: {
-        webcomponents: {
-            title: 'DotCMS WebComponents',
-            url: 'https://dotcms.github.io/core-web/dotcms-webcomponents'
-        },
-        blockeditor: {
-            title: 'DotCMS Block Editor',
-            url: 'https://dotcms.github.io/core-web/block-editor'
-        }
-    }
+    addons: ['@storybook/addon-knobs/register']
 };
--- apps/dotcms-ui/.storybook/main.js
+++ apps/dotcms-ui/.storybook/main.js
@@ -2,8 +2,18 @@ const rootMain = require('../../../.storybook/main');
 
 // Use the following syntax to add addons!
 // rootMain.addons.push('');
-rootMain.stories.push(
-    ...['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)']
-);
+rootMain.stories.push(...['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)']);
 
-module.exports = rootMain;
+module.exports = {
+    ...rootMain,
+    refs: {
+        webcomponents: {
+            title: 'DotCMS WebComponents',
+            url: 'https://dotcms.github.io/core-web/dotcms-webcomponents'
+        },
+        blockeditor: {
+            title: 'DotCMS Block Editor',
+            url: 'https://dotcms.github.io/core-web/block-editor'
+        }
+    }
+};
