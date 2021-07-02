5508287ba1b8f0e62e329dd6f26f9fde00cc6332
Alfredo Li <alfredo@dotcms.com>
2021-07-02 15:32:07 -0600

    change3

--- apps/dotcms-ui/src/app/portlets/dot-edit-page/content/services/html/libraries/iframe-edit-mode.js.ts
+++ apps/dotcms-ui/src/app/portlets/dot-edit-page/content/services/html/libraries/iframe-edit-mode.js.ts
@@ -69,6 +69,9 @@ export const EDIT_PAGE_JS = `
         }
     });
 
+
+
+    
     drake.on('drag', function() {
         window.requestAnimationFrame(function() {
             const el = document.querySelector('.gu-mirror');
