53cc0e38d6016b7f1009f59d08da3d21f236c354
Freddy Montes <freddymontes@gmail.com>
2021-07-15 14:47:31 -0600

    Style and add controlvalueaccesor

--- apps/dotcms-ui/src/app/view/components/_common/dot-md-icon-selector/dot-md-icon-selector.component.ts
+++ apps/dotcms-ui/src/app/view/components/_common/dot-md-icon-selector/dot-md-icon-selector.component.ts
@@ -1,19 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
     selector: 'dot-md-icon-selector',
     templateUrl: './dot-md-icon-selector.component.html',
-    styleUrls: ['./dot-md-icon-selector.component.scss']
+    styleUrls: ['./dot-md-icon-selector.component.scss'],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => DotMdIconSelectorComponent),
+            multi: true
+        }
+    ]
 })
-export class DotMdIconSelectorComponent implements OnInit {
+export class DotMdIconSelectorComponent implements ControlValueAccessor {
+    value = '';
 
-    constructor() { }
+    constructor() {}
 
-    ngOnInit(): void {
+    onTouched = () => {};
+    onChange = (_) => {};
+
+    onBlur() {
+        this.onTouched();
     }
 
-    onChange(e) {
-        console.log(e);
+    registerOnTouched(fn: any) {
+        this.onTouched = fn;
     }
 
+    registerOnChange(fn: any) {
+        this.onChange = fn;
+    }
+
+    writeValue(value: string) {
+        this.value = value;
+    }
+
+    /**
+     * Handle web component icon selection
+     *
+     * @param {CustomEvent<string>} e
+     * @memberof DotMdIconSelectorComponent
+     */
+    onSelect(e: CustomEvent<string>) {
+        this.onChange((e.target as HTMLInputElement).value);
+    }
 }
