a734a747218c1861d88d9143a01614646b35884a
Alfredo Li <alfredo@dotcms.com>
2021-07-14 19:05:43 -0600

    Add placeholder to drag and drop images - progress

--- apps/dotcms-ui/src/app/portlets/dot-edit-page/content/services/html/libraries/iframe-edit-mode.js.ts
+++ apps/dotcms-ui/src/app/portlets/dot-edit-page/content/services/html/libraries/iframe-edit-mode.js.ts
@@ -143,6 +143,140 @@ export const EDIT_PAGE_JS = `
             return this.down && drake.dragging;
         }
     });
+
+    // D&D Img - Start
+
+    function isCursorOnUpperSide(cursor, contentletBoundingRect) {
+        return cursor.y - contentletBoundingRect.top  <  (contentletBoundingRect.bottom - contentletBoundingRect.top)/2
+    }
+
+    function isContentletPlaceholderInDOM() {
+        return document.getElementById('contentletPlaceholder');
+    }
+
+    function isContainerAndContentletValid(container, contentlet) {
+        return !container.classList.contains('no') && !contentlet.classList.contains('gu-transit');
+    }
+
+    function insertBeforeElement(newElem, element) {
+        element.parentNode.insertBefore(newElem, element);
+    }
+
+    function insertAfterElement(newElem, element) {
+        try{
+            element.parentNode.insertBefore(newElem, element.nextSibling);
+        }catch(error){}
+    }
+
+    function removeElementById(elemId) {
+        document.getElementById(elemId).remove()
+    }
+
+    function checkIfContainerAllowsContent(event) {
+
+        var container = event.target.closest('[data-dot-object="container"]');
+        
+        // Different than 1 file
+        if (event.dataTransfer.items.length !== 1 ) {
+            return false;
+        }
+
+        // File uploaded not an img
+        if (!event.dataTransfer.items[0].type.match(/image/g) ) {
+            return false;
+        }
+
+        // Container does NOT allow img
+        if (!container.dataset.dotAcceptTypes.toLocaleLowerCase().match(/image/g)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    var contentletPlaceholder;
+    var currentContainer;
+
+    window.addEventListener("dragenter", (event) => {
+        
+        event.preventDefault(); 
+        event.stopPropagation();
+
+        var container = event.target.closest('[data-dot-object="container"]');
+        currentContainer = container;
+
+        if (container && !checkIfContainerAllowsContent(event)) {
+            container.classList.add('no');
+        }
+
+    }, false);
+
+    window.addEventListener('dragover', (event) => {
+
+        event.preventDefault(); 
+        event.stopPropagation();
+
+        var container = event.target.closest('[data-dot-object="container"]');
+        var contentlet = event.target.closest('[data-dot-object="contentlet"]');
+
+        if (contentlet) {
+
+            if (isContainerAndContentletValid(container, contentlet) && isContentletPlaceholderInDOM()) { 
+                removeElementById('contentletPlaceholder');
+            }
+
+            contentletPlaceholder = document.createElement('div');
+            contentletPlaceholder.id = 'contentletPlaceholder';
+            contentletPlaceholder.setAttribute('data-dot-object', 'contentlet')
+            contentletPlaceholder.classList.add('gu-transit')
+            
+            if (isContainerAndContentletValid(container, contentlet) && isCursorOnUpperSide(event, contentlet.getBoundingClientRect())) {
+                insertBeforeElement(contentletPlaceholder, contentlet);
+            } else if (isContainerAndContentletValid(container, contentlet)) {
+                insertAfterElement(contentletPlaceholder, contentlet);
+            }
+
+            contentletPlaceholder = null;
+
+        }
+
+    }, false)
+
+    window.addEventListener('dragleave', (event) => {
+        event.preventDefault(); 
+        event.stopPropagation();
+
+        var container = event.target.closest('[data-dot-object="container"]');
+
+        if (container && currentContainer !== container) {
+            container.classList.remove('no');
+        }
+
+    }, false)
+
+    window.addEventListener('drop', (event) => {
+        event.preventDefault(); 
+        event.stopPropagation();
+
+        var container = event.target.closest('[data-dot-object="container"]');
+
+        if (container && !container.classList.contains('no')) {
+            console.log('** INSERT')
+        } else {
+            console.log('** NO INSERT')
+        }
+
+        if (isContentletPlaceholderInDOM()) {
+                removeElementById('contentletPlaceholder');
+        }
+        
+        if (container) {
+            container.classList.remove('no');
+        }
+
+    }, false)
+
+    // D&D Img - End
 })();
 
 `;
