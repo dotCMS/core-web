18afcecf78d68421558c10ae207138f4ebe99c15
Freddy Montes <freddymontes@gmail.com>
2021-07-30 14:29:06 -0600

    Add basic suggestion list

---
+++ libs/block-editor/src/lib/extensions/components/suggestions-list-item/suggestions-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dotcms-suggestions-list-item',
+  templateUrl: './suggestions-list-item.component.html',
+  styleUrls: ['./suggestions-list-item.component.scss']
+})
+export class SuggestionsListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
