91b8fd03cd922da1fa1605c6d12c381fc248124e
Rafael <rjvelazco21@gmail.com>
2021-09-07 09:43:59 -0400

    add httpClient in suggestions service

--- libs/block-editor/src/lib/extensions/services/suggestions.service.ts
+++ libs/block-editor/src/lib/extensions/services/suggestions.service.ts
@@ -4,26 +4,34 @@ import { Observable } from 'rxjs';
 import { pluck } from 'rxjs/operators';
 
 import { DotCMSContentlet, DotCMSContentType } from '@dotcms/dotcms-models';
-import { CoreWebService } from '@dotcms/dotcms-js';
+// eslint-disable-next-line max-len
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class SuggestionsService {
-    constructor(private coreWebSerice: CoreWebService) {}
+    private headers = new HttpHeaders();
+
+    constructor(private http: HttpClient) {
+        this.headers
+                .set('Accept', '*/*')
+                .set('Content-Type', 'application/json');
+    }
 
     getContentTypes(filter = ''): Observable<DotCMSContentType[]> {
-        return this.coreWebSerice
-            .requestView({
-                url: `/api/v1/contenttype?filter=${filter}&orderby=modDate&direction=DESC&per_page=40`
+        return this.http
+            .get(`/api/v1/contenttype?filter=${filter}&orderby=modDate&direction=DESC&per_page=40`, {
+                headers: this.headers
             })
             .pipe(pluck('entity'));
     }
 
     getContentlets(contentType = ''): Observable<DotCMSContentlet[]> {
-        return this.coreWebSerice
-            .requestView({
-                // eslint-disable-next-line max-len
-                url: `/api/content/render/false/query/+contentType:${contentType}%20+languageId:1%20+deleted:false%20+working:true/orderby/modDate%20desc`
+        return this.http
+            // eslint-disable-next-line max-len
+            .get(`/api/content/render/false/query/+contentType:${contentType}%20+languageId:1%20+deleted:false%20+working:true/orderby/modDate%20desc`, {
+                headers: this.headers
             })
             .pipe(pluck('contentlets'));
     }
+
 }
