7765aee4065902a8595df532c793645efb6daa73
Rafael <rjvelazco21@gmail.com>
2021-11-04 14:37:13 -0400

    remove unused imports

--- libs/block-editor/src/lib/extensions/components/bubble-menu/bubble-menu.component.ts
+++ libs/block-editor/src/lib/extensions/components/bubble-menu/bubble-menu.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Editor, EditorEvents } from '@tiptap/core';
+import { Editor } from '@tiptap/core';
 
 export interface BubbleMenuItem {
     icon: string;
