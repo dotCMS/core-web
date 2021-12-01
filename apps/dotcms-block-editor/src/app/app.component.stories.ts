c7a836f540329fb59b537d4c2697ad986cacf2ac
Freddy Montes <freddymontes@gmail.com>
2021-12-01 10:07:37 -0600

    Add storybook to dotcms-block-editor

---
+++ apps/dotcms-block-editor/src/app/app.component.stories.ts
@@ -0,0 +1,15 @@
+
+import { AppComponent } from './app.component';
+
+export default {
+  title: 'AppComponent'
+}
+
+export const primary = () => ({
+  moduleMetadata: {
+    imports: []
+  },
+  component: AppComponent,
+  props: {
+  }
+})
