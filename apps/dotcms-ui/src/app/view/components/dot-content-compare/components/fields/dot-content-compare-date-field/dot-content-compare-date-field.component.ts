06a9f4c0829b60cf07cb096df9f0e78a245baf0d
hmoreras <humberto.morera@dotcms.com>
2021-12-08 15:52:21 -0600

    most of the fields

--- apps/dotcms-ui/src/app/view/components/dot-content-compare/components/fields/dot-content-compare-date-field/dot-content-compare-date-field.component.ts
+++ apps/dotcms-ui/src/app/view/components/dot-content-compare/components/fields/dot-content-compare-date-field/dot-content-compare-date-field.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DotFormatDateService } from '@services/dot-format-date-service';
+
+export enum FieldWhiteList {
+    Text = 'Text',
+    Textarea = 'Textarea',
+    Checkbox = 'Checkbox',
+    'Constant-Field' = 'Constant-Field',
+    'Key-Value' = 'Key-Value',
+    Radio = 'Radio',
+    Select = 'Select',
+    'Multi-Select' = 'Multi-Select',
+    Tag = 'Tag',
+    'Custom-Field' = 'Custom-Field',
+    'Hidden-Field' = 'Hidden-Field',
+    Image = 'image',
+    File = 'File',
+    Binary = 'Binary',
+    Category = 'Category',
+
+    'Date-and-Time' = 'Date-and-Time',
+    Time = 'Time',
+    'WYSIWYG' = 'WYSIWYG',
+    'Host-Folder' = 'Host-Folder'
+}
+
+enum DateType {
+    date = 'MM/dd/yyyy',
+    Time = 'hh:mm',
+    'Date-and-Time' = 'MM/dd/yyyy - hh:mm'
+}
 
 @Component({
-  selector: 'dot-dot-content-compare-date-field',
-  templateUrl: './dot-content-compare-date-field.component.html',
-  styleUrls: ['./dot-content-compare-date-field.component.scss']
+    selector: 'dot-content-compare-date-field',
+    templateUrl: './dot-content-compare-date-field.component.html',
+    styleUrls: ['./dot-content-compare-date-field.component.scss']
 })
 export class DotContentCompareDateFieldComponent implements OnInit {
+    constructor(private dotFormatDateService: DotFormatDateService) {}
 
-  constructor() { }
+    // @Input() set value(value: string) {
+    //     // this.value = this.dotFormatDateService.format(new Date(value), DateType[this.format]);
+    // }
+    @Input() value: string;
+    @Input() format: string;
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
