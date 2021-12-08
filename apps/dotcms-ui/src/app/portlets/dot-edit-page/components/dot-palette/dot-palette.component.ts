b586a8c1da37269d5c18862c6ab990d66607df78
Freddy Montes <freddymontes@gmail.com>
2021-12-08 07:07:24 -0600

    Add basic angular animation

--- apps/dotcms-ui/src/app/portlets/dot-edit-page/components/dot-palette/dot-palette.component.ts
+++ apps/dotcms-ui/src/app/portlets/dot-edit-page/components/dot-palette/dot-palette.component.ts
@@ -1,17 +1,35 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, Input } from '@angular/core';
 import { DotCMSContentType } from '@dotcms/dotcms-models';
 
 @Component({
     selector: 'dot-palette',
     templateUrl: './dot-palette.component.html',
-    styleUrls: ['./dot-palette.component.scss']
+    styleUrls: ['./dot-palette.component.scss'],
+    animations: [
+        trigger('inOut', [
+            state(
+                'contentlet',
+                style({
+                    transform: 'translateX(-100%)'
+                })
+            ),
+            state(
+                'type',
+                style({
+                    transform: 'translateX(100%)'
+                })
+            ),
+            transition('type => contentlet', animate('250ms')),
+            transition('contentlet => type', animate('250ms'))
+        ])
+    ]
 })
 export class DotPaletteComponent {
     @Input() items: DotCMSContentType[] = [];
     @Input() languageId: string;
-    contentTypeVariable: string = '';
-
-    constructor() {}
+    contentTypeVariable = '';
+    state: 'type' | 'contentlet' = 'type';
 
     /**
      * Sets value on contentTypeVariable variable to show/hide components on the UI
@@ -21,5 +39,6 @@ export class DotPaletteComponent {
      */
     switchView(variableName?: string): void {
         this.contentTypeVariable = variableName ? variableName : '';
+        this.state = variableName ? 'contentlet' : 'type';
     }
 }
