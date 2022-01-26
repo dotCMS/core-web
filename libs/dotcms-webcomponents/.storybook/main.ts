8fff2cb1bef616ad87117c0875b06f1c6a0e5a83
Freddy Montes <freddymontes@gmail.com>
2022-01-26 11:16:13 -0600

    Update dot-binary-file.stories

--- libs/dotcms-webcomponents/.storybook/main.ts
+++ libs/dotcms-webcomponents/.storybook/main.ts
@@ -9,7 +9,7 @@ module.exports = {
         ...rootMain.stories,
         // '../src/**/*.stories.mdx',
         // '../src/**/*.stories.@(js|jsx|ts|tsx)'
-        '../src/collections/dot-card-view/*.stories.@(js|jsx|ts|tsx)'
+        '../src/components/contenttypes-fields/dot-binary-file/dot-binary-file.stories.js'
     ],
     addons: [...rootMain.addons],
     webpackFinal: async (config, { configType }) => {
