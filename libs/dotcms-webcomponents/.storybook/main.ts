be385fb24736d061b0a401ccd8cea181fe9bbfba
Freddy Montes <freddymontes@gmail.com>
2022-01-26 15:53:10 -0600

    Update dot-select

--- libs/dotcms-webcomponents/.storybook/main.ts
+++ libs/dotcms-webcomponents/.storybook/main.ts
@@ -9,7 +9,7 @@ module.exports = {
         ...rootMain.stories,
         // '../src/**/*.stories.mdx',
         // '../src/**/*.stories.@(js|jsx|ts|tsx)'
-        '../src/components/contenttypes-fields/dot-radio/*.stories.@(js|jsx|ts|tsx)'
+        '../src/components/contenttypes-fields/dot-select/*.stories.@(js|jsx|ts|tsx)'
     ],
     addons: [...rootMain.addons],
     webpackFinal: async (config, { configType }) => {
