621ffc3b905f5a9652bf091e23c9311462f3c5d4
Freddy Montes <freddymontes@gmail.com>
2022-01-27 08:54:52 -0600

    dot-data-view-button.stories

--- libs/dotcms-webcomponents/src/components/dot-data-view-button/dot-data-view-button.stories.js
+++ libs/dotcms-webcomponents/src/components/dot-data-view-button/dot-data-view-button.stories.js
@@ -3,8 +3,15 @@ import readme from './readme.md';
 export default {
     title: 'Components',
     parameters: {
-        notes: readme
-    }
+        docs: {
+            title: 'Data view button',
+            description: {
+                component:
+                    'A group button to select data view options.',
+            },
+            page: readme
+        },
+    },
 };
 
 export const DataViewButton = () => `<dot-data-view-button value="list" />`;
