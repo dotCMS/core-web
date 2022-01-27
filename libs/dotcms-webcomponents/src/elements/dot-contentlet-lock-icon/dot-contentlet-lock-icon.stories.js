72172b063becb54d845b771e69d8c3e5b9cf392f
Freddy Montes <freddymontes@gmail.com>
2022-01-27 12:19:32 -0600

    dot-contentlet-lock-icon stories

--- libs/dotcms-webcomponents/src/elements/dot-contentlet-lock-icon/dot-contentlet-lock-icon.stories.js
+++ libs/dotcms-webcomponents/src/elements/dot-contentlet-lock-icon/dot-contentlet-lock-icon.stories.js
@@ -1,49 +1,30 @@
-import { withKnobs, text } from '@storybook/addon-knobs';
 import readme from './readme.md';
 
 export default {
-    title: 'Elements/Contentlet Lock Icon',
-    decorators: [withKnobs],
+    title: 'Elements',
     parameters: {
-        notes: readme
+        docs: {
+            title: 'Contentlet Lock Icon',
+            description: {
+                component:
+                    'Lock icon'
+            },
+            page: readme
+        },
+    },
+    args: {
+        locked: true
     }
 };
 
-export const Lock = () => {
-    const props = [
-        {
-            name: 'contentlet',
-            content: text('Contentlet', {
-                locked: 'true'
-            })
-        }
-    ];
-
+const Template = (args) => {
     const contentletLockIcon = document.createElement('dot-contentlet-lock-icon');
 
-    props.forEach(({ name, content }) => {
-        contentletLockIcon[name] = content;
-    });
+    for (const item in args) {
+        contentletLockIcon[item] = args[item];
+    }
 
     return contentletLockIcon;
 };
 
-export const Unlock = () => {
-    const props = [
-        {
-            name: 'contentlet',
-            content: text('Contentlet', {
-                locked: 'false'
-            })
-        }
-    ];
-
-    const contentletLockIcon = document.createElement('dot-contentlet-lock-icon');
-
-    props.forEach(({ name, content }) => {
-        contentletLockIcon[name] = content;
-    });
-
-    return contentletLockIcon;
-};
-
+export const Icon = Template.bind({});
