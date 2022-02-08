f52a98de7436b2df6acecd1641d4a26f1e7297d9
Alfredo Li <alfredo@dotcms.com>
2022-02-08 13:22:21 -0600

    dotCMS/core#21659 fix Invalidate localstorage messages keys when dotcms updates

--- apps/dotcms-ui/src/app/app.component.spec.ts
+++ apps/dotcms-ui/src/app/app.component.spec.ts
@@ -3,14 +3,27 @@
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
-import { ComponentFixture } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
-import { DOTTestBed } from './test/dot-test-bed';
 import { DotUiColorsService } from '@services/dot-ui-colors/dot-ui-colors.service';
-import { DotcmsConfigService } from '@dotcms/dotcms-js';
+import {
+    CoreWebService,
+    CoreWebServiceMock,
+    DotcmsConfigService,
+    LoggerService,
+    StringUtils
+} from '@dotcms/dotcms-js';
 import { of } from 'rxjs';
 import { DotMessageService } from '@services/dot-message/dot-messages.service';
+import { DotLocalstorageService } from '@services/dot-localstorage/dot-localstorage.service';
+import { DotNavLogoService } from '@services/dot-nav-logo/dot-nav-logo.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+class DotLocalstorageServiceMock {
+    getItem(_key: string) {}
+    setItem(_key: string, _value: string) {}
+}
 
 describe('AppComponent', () => {
     let fixture: ComponentFixture<AppComponent>;
@@ -18,19 +31,30 @@ describe('AppComponent', () => {
     let dotCmsConfigService: DotcmsConfigService;
     let dotUiColorsService: DotUiColorsService;
     let dotMessageService: DotMessageService;
+    let dotLocalstorageService: DotLocalstorageService;
 
     beforeEach(() => {
-        DOTTestBed.configureTestingModule({
+        TestBed.configureTestingModule({
             declarations: [AppComponent],
-            imports: [RouterTestingModule],
-            providers: []
+            imports: [RouterTestingModule, HttpClientTestingModule],
+            providers: [
+                { provide: DotLocalstorageService, useClass: DotLocalstorageServiceMock },
+                { provide: CoreWebService, useClass: CoreWebServiceMock },
+                DotUiColorsService,
+                DotNavLogoService,
+                DotcmsConfigService,
+                LoggerService,
+                StringUtils
+            ]
         });
 
-        fixture = DOTTestBed.createComponent(AppComponent);
+        fixture = TestBed.createComponent(AppComponent);
         de = fixture.debugElement;
+
         dotCmsConfigService = de.injector.get(DotcmsConfigService);
         dotUiColorsService = de.injector.get(DotUiColorsService);
         dotMessageService = de.injector.get(DotMessageService);
+        dotLocalstorageService = de.injector.get(DotLocalstorageService);
 
         spyOn<any>(dotCmsConfigService, 'getConfig').and.returnValue(
             of({
@@ -38,26 +62,37 @@ describe('AppComponent', () => {
                     primary: '#123',
                     secondary: '#456',
                     background: '#789'
+                },
+                releaseInfo: {
+                    buildDate: 'Jan 1, 2022'
                 }
             })
         );
-
         spyOn(dotUiColorsService, 'setColors');
-
         spyOn(dotMessageService, 'init');
-
-        fixture.detectChanges();
     });
 
-    it('should init message service', () => {
+    it('should init message service without force loading', () => {
+        spyOn(dotLocalstorageService, 'getItem').and.returnValue('Jan 1, 2022');
+        fixture.detectChanges();
         expect(dotMessageService.init).toHaveBeenCalledWith(false);
     });
 
+    it('should init message service forcing loading', () => {
+        spyOn(dotLocalstorageService, 'getItem').and.returnValue('Dic 1, 2021');
+        spyOn(dotLocalstorageService, 'setItem').and.callThrough();
+        fixture.detectChanges();
+        expect(dotMessageService.init).toHaveBeenCalledWith(true);
+        expect(dotLocalstorageService.setItem).toHaveBeenCalledWith('buildDate', 'Jan 1, 2022');
+    });
+
     it('should have router-outlet', () => {
+        fixture.detectChanges();
         expect(de.query(By.css('router-outlet')) !== null).toBe(true);
     });
 
     it('should set ui colors', () => {
+        fixture.detectChanges();
         expect(dotUiColorsService.setColors).toHaveBeenCalledWith(jasmine.any(HTMLElement), {
             primary: '#123',
             secondary: '#456',
--- apps/dotcms-ui/src/app/app.component.ts
+++ apps/dotcms-ui/src/app/app.component.ts
@@ -4,34 +4,62 @@ import { DotUiColorsService } from '@services/dot-ui-colors/dot-ui-colors.servic
 import { take, map } from 'rxjs/operators';
 import { DotMessageService } from '@services/dot-message/dot-messages.service';
 import { DotNavLogoService } from '@services/dot-nav-logo/dot-nav-logo.service';
+import { DotLocalstorageService } from '@services/dot-localstorage/dot-localstorage.service';
 @Component({
     selector: 'dot-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+    private BUILDATE_LOCALSTORAGE_KEY = 'buildDate';
 
     constructor(
         private dotCmsConfigService: DotcmsConfigService,
         private dotUiColors: DotUiColorsService,
         private dotMessageService: DotMessageService,
-        private dotNavLogoService: DotNavLogoService
+        private dotNavLogoService: DotNavLogoService,
+        private dotLocalstorageService: DotLocalstorageService
     ) {}
 
     ngOnInit() {
-        this.dotMessageService.init(false);
         this.dotCmsConfigService
             .getConfig()
             .pipe(
                 take(1),
-                map((config: ConfigParams) => ({
-                    colors: config.colors,
-                    navBar: config.logos?.navBar
-                }))
+                map((config: ConfigParams) => {
+                    return {
+                        buildDate: config.releaseInfo?.buildDate,
+                        colors: config.colors,
+                        navBar: config.logos?.navBar
+                    };
+                })
             )
-            .subscribe(({ colors, navBar }: { colors: DotUiColors; navBar: string }) => {
-                this.dotNavLogoService.setLogo(navBar);
-                this.dotUiColors.setColors(document.querySelector('html'), colors);
-            });
+            .subscribe(
+                ({
+                    buildDate,
+                    colors,
+                    navBar
+                }: {
+                    buildDate: string;
+                    colors: DotUiColors;
+                    navBar: string;
+                }) => {
+                    if (
+                        this.dotLocalstorageService.getItem(this.BUILDATE_LOCALSTORAGE_KEY) ===
+                        buildDate
+                    ) {
+                        this.dotMessageService.init(false);
+                    } else {
+                        this.dotMessageService.init(true);
+                        this.dotLocalstorageService.setItem(
+                            this.BUILDATE_LOCALSTORAGE_KEY,
+                            buildDate
+                        );
+                    }
+
+                    this.dotNavLogoService.setLogo(navBar);
+                    this.dotUiColors.setColors(document.querySelector('html'), colors);
+                }
+            );
     }
 }
