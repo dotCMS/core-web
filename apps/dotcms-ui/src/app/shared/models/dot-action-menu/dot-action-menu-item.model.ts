b82e031e79ae4d3cd39a68487b22d9ed60c820f0
Freddy Montes <freddymontes@gmail.com>
2022-02-16 12:50:08 -0600

    Round 2

--- apps/dotcms-ui/src/app/shared/models/dot-action-menu/dot-action-menu-item.model.ts
+++ apps/dotcms-ui/src/app/shared/models/dot-action-menu/dot-action-menu-item.model.ts
@@ -1,6 +1,6 @@
 import { MenuItem } from 'primeng/api';
 
 export interface DotActionMenuItem {
-    shouldShow?: (x?: any) => boolean;
+    shouldShow?: (x?: unknown) => boolean;
     menuItem: MenuItem;
 }
