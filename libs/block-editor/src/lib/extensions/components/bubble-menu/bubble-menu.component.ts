7e9d910cf59e689f258add9d5b883b411fc5c56c
Rafael <rjvelazco21@gmail.com>
2022-03-03 14:32:28 -0400

    progress

--- libs/block-editor/src/lib/extensions/components/bubble-menu/bubble-menu.component.ts
+++ libs/block-editor/src/lib/extensions/components/bubble-menu/bubble-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { Editor } from '@tiptap/core';
 import { bubbleMenuItems, bubbleMenuImageItems } from '@dotcms/block-editor';
 
@@ -16,20 +16,17 @@ export interface BubbleMenuItem {
 })
 export class BubbleMenuComponent implements OnInit, OnDestroy {
     @Input() editor: Editor;
+    @Input() items: BubbleMenuItem[] = [];
+    @Output() editorCommand: EventEmitter<any> = new EventEmitter();
 
-    public enabledMarks: string[] = [];
     public textAlings: string[] = ['left', 'center', 'right'];
-    public activeMarks: string[] = [];
-
-    public items: BubbleMenuItem[] = [];
 
     ngOnInit() {
-        this.setEnabledMarks();
+        // this.setEnabledMarks();
 
         /**
          * Every time the editor is updated, the active state of the buttons must be updated.
          */
-        this.editor.on('transaction', this.onUpdate.bind(this));
 
         /**
          * Every time the selection is updated, check if it's a dotImage
@@ -38,74 +35,11 @@ export class BubbleMenuComponent implements OnInit, OnDestroy {
     }
 
     ngOnDestroy(): void {
-        this.editor.off('transaction', this.onUpdate.bind(this));
         this.editor.off('selectionUpdate', this.setMenuItems.bind(this));
-    }
-
-    command(item: BubbleMenuItem): void {
-        this.menuActions(item);
-        this.setActiveMarks();
-        this.updateActiveItems();
     }
 
     preventDeSelection(event: MouseEvent): void {
         event.preventDefault();
-    }
-
-    private menuActions(item: BubbleMenuItem): void {
-        const markActions = {
-            bold: () => {
-                this.editor.commands.toggleBold();
-            },
-            italic: () => {
-                this.editor.commands.toggleItalic();
-            },
-            strike: () => {
-                this.editor.commands.toggleStrike();
-            },
-            underline: () => {
-                this.editor.commands.toggleUnderline();
-            },
-            left: () => {
-                this.toggleTextAlign('left', item.active);
-            },
-            center: () => {
-                this.toggleTextAlign('center', item.active);
-            },
-            right: () => {
-                this.toggleTextAlign('right', item.active);
-            },
-            bulletList: () => {
-                this.editor.commands.toggleBulletList();
-            },
-            orderedList: () => {
-                this.editor.commands.toggleOrderedList();
-            },
-            indent: () => {
-                if (this.isListNode()) {
-                    this.editor.commands.sinkListItem('listItem');
-                }
-            },
-            outdent: () => {
-                if (this.isListNode()) {
-                    this.editor.commands.liftListItem('listItem');
-                }
-            },
-            link: () => {
-                this.editor.commands.toogleLinkForm();
-            },
-            clearAll: () => {
-                this.editor.commands.unsetAllMarks();
-                this.editor.commands.clearNodes();
-            }
-        };
-
-        markActions[item.markAction] ? markActions[item.markAction]() : null;
-    }
-
-    private onUpdate(): void {
-        this.setActiveMarks();
-        this.updateActiveItems();
     }
 
     private setMenuItems({ editor: { state } }: { editor: Editor }): void {
@@ -122,16 +56,6 @@ export class BubbleMenuComponent implements OnInit, OnDestroy {
         this.items = isDotImage ? bubbleMenuImageItems : bubbleMenuItems;
     }
 
-    private updateActiveItems(): void {
-        this.items.forEach((item) => {
-            if (this.activeMarks.includes(item.markAction)) {
-                item.active = true;
-            } else {
-                item.active = false;
-            }
-        });
-    }
-
     private toggleTextAlign(aligment: string, active: boolean): void {
         if (active) {
             this.editor.commands.unsetTextAlign();
@@ -142,19 +66,5 @@ export class BubbleMenuComponent implements OnInit, OnDestroy {
 
     private isListNode(): boolean {
         return this.editor.isActive('bulletList') || this.editor.isActive('orderedList');
-    }
-
-    private setEnabledMarks(): void {
-        this.enabledMarks = [
-            ...Object.keys(this.editor.schema.marks),
-            ...Object.keys(this.editor.schema.nodes)
-        ];
-    }
-
-    private setActiveMarks(): void {
-        this.activeMarks = [
-            ...this.enabledMarks.filter((mark) => this.editor.isActive(mark)),
-            ...this.textAlings.filter((alignment) => this.editor.isActive({ textAlign: alignment }))
-        ];
     }
 }
--- libs/block-editor/src/lib/plugins/dot-bubble-menu.plugin.ts
+++ libs/block-editor/src/lib/plugins/dot-bubble-menu.plugin.ts
@@ -1,4 +1,4 @@
-import { BubbleMenuPluginProps } from '@tiptap/extension-bubble-menu';
+import { BubbleMenuPluginProps, BubbleMenuViewProps } from '@tiptap/extension-bubble-menu';
 import { EditorView } from 'prosemirror-view';
 import { isNodeSelection, posToDOMRect } from '@tiptap/core';
 import { PluginKey, Plugin, EditorState } from 'prosemirror-state';
@@ -6,8 +6,19 @@ import { BubbleMenuView } from '@tiptap/extension-bubble-menu';
 
 // Utils
 import { getNodePosition } from '@dotcms/block-editor';
+import { ComponentRef } from '@angular/core';
+import { BubbleMenuItem } from '../extensions/components/bubble-menu/bubble-menu.component';
+import { bubbleMenuImageItems, bubbleMenuItems } from '../utils/bubble-menu.utils';
 
-export const DotBubbleMenuPlugin = (options: BubbleMenuPluginProps) => {
+export declare type DotBubbleMenuPluginProps<T = any> = BubbleMenuPluginProps & {
+    component: ComponentRef<T>;
+};
+
+export declare type DotBubbleMenuViewProps<T = any> = BubbleMenuViewProps & {
+    component: ComponentRef<T>;
+};
+
+export const DotBubbleMenuPlugin = (options: DotBubbleMenuPluginProps) => {
     return new Plugin({
         key:
             typeof options.pluginKey === 'string'
@@ -18,8 +29,22 @@ export const DotBubbleMenuPlugin = (options: BubbleMenuPluginProps) => {
 };
 
 export class DotBubbleMenuPluginView extends BubbleMenuView {
+    public component: ComponentRef<any>;
+
+    /* @Overrrider */
+    constructor(props: DotBubbleMenuViewProps) {
+        // Inherit the parent class
+        super(props);
+
+        // New Properties
+        this.component = props.component;
+        this.component.instance.items = bubbleMenuItems;
+        this.subscribeComponentEvents();
+    }
+
     /* @Overrrider */
     update(view: EditorView, oldState?: EditorState) {
+        this.updateComponent();
         const { state, composing } = view;
         const { doc, selection } = state;
         const isSame = oldState && oldState.doc.eq(doc) && oldState.selection.eq(selection);
@@ -67,4 +92,89 @@ export class DotBubbleMenuPluginView extends BubbleMenuView {
 
         this.show();
     }
+
+    updateComponent() {
+        const { items } = this.component.instance;
+        const activeMarks = setActiveMarks(this.editor);
+        this.component.instance.items = updateActiveItems(items, activeMarks);
+        this.component.changeDetectorRef.detectChanges();
+    }
+
+    /* Custom Functions */
+    subscribeComponentEvents() {
+        this.component.instance.editorCommand.subscribe((event: BubbleMenuItem) => {
+            bubbleMenuAction(this.editor, event.markAction);
+        });
+    }
 }
+
+// Temporal
+const bubbleMenuAction = (editor, action) => {
+    switch (action) {
+        case 'bold':
+            editor.commands.toggleBold();
+            break;
+        case 'italic':
+            editor.commands.toggleItalic();
+            break;
+        case 'strike':
+            editor.commands.toggleStrike();
+            break;
+        case 'underline':
+            editor.commands.toggleUnderline();
+            break;
+        // case 'left':
+        //     this.toggleTextAlign('left', item.active);
+        // break;
+        // case 'center':
+        //     this.toggleTextAlign('center', item.active);
+        // break;
+        // case 'right':
+        //     this.toggleTextAlign('right', item.active);
+        // break;
+        case 'bulletList':
+            editor.commands.toggleBulletList();
+            break;
+        case 'orderedList':
+            editor.commands.toggleOrderedList();
+            break;
+        // case 'indent':
+        //     if (this.isListNode()) {
+        //         this.editor.commands.sinkListItem('listItem');
+        //     }
+        // break;
+        // case 'outdent':
+        //     if (this.isListNode()) {
+        //         this.editor.commands.liftListItem('listItem');
+        //     }
+        // break;
+        case 'link':
+            editor.commands.toogleLinkForm();
+            break;
+        case 'clearAll':
+            editor.commands.unsetAllMarks();
+            editor.commands.clearNodes();
+            break;
+    }
+};
+
+const enabledMarks = (editor): string[] => {
+    return [...Object.keys(editor.schema.marks), ...Object.keys(editor.schema.nodes)];
+};
+
+const setActiveMarks = (editor): string[] => {
+    return [
+        ...enabledMarks(editor).filter((mark) => editor.isActive(mark))
+        // ...this.textAlings.filter((alignment) => this.editor.isActive({ textAlign: alignment }))
+    ];
+};
+
+const updateActiveItems = (
+    items: BubbleMenuItem[] = [],
+    activeMarks: string[]
+): BubbleMenuItem[] => {
+    return items.map((item) => {
+        item.active = activeMarks.includes(item.markAction);
+        return item;
+    });
+};
