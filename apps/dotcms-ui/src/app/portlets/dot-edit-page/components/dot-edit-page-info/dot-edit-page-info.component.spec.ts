36a0714afd950120b53079e638db2cd91b7b1aa1
erickgonzalez <erick.gonzalez@dotcms.com>
2023-07-31 13:42:46 -0600

    fix test

--- apps/dotcms-ui/src/app/portlets/dot-edit-page/components/dot-edit-page-info/dot-edit-page-info.component.spec.ts
+++ apps/dotcms-ui/src/app/portlets/dot-edit-page/components/dot-edit-page-info/dot-edit-page-info.component.spec.ts
@@ -41,7 +41,7 @@ describe('DotEditPageInfoComponent', () => {
 
     describe('default', () => {
         beforeEach(() => {
-            component.apiLink = 'api/v1/page/render/an/url/test?language_id=1';
+            component.apiLink = '/api/v1/page/render/an/url/test?language_id=1';
             component.title = 'A title';
             component.url = 'http://demo.dotcms.com:9876/an/url/test';
             fixture.detectChanges();
@@ -55,7 +55,7 @@ describe('DotEditPageInfoComponent', () => {
         it('should have api link', () => {
             const apiLink: DebugElement = de.query(By.css('dot-api-link'));
             expect(apiLink.componentInstance.link).toBe(
-                'api/v1/page/render/an/url/test?language_id=1'
+                '/api/v1/page/render/an/url/test?language_id=1'
             );
         });
 
